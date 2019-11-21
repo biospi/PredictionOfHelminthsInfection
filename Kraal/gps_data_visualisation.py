cf29283b60a4789e4e7b5bad536f379dbe6743a0
fo18103 <Mojjo@2015>
2019-11-21 21:53:42 +0000

    added dump famacha data to json file.

--- Kraal/gps_data_visualisation.py
+++ Kraal/gps_data_visualisation.py
@@ -7,9 +7,37 @@ from mpl_toolkits.basemap import Basemap
 from datetime import datetime
 import traceback
 import imageio
+import os.path
+import glob
+from sys import exit
+from colour import Color
+
+
+def uniqueish_color():
+    """There're better ways to generate unique colors, but this isn't awful."""
+    return plt.cm.gist_ncar(np.random.random())
+
+
+def images_to_gif_():
+    filenames = []
+    # os.chdir("/mydir")
+    for file in glob.glob("*.png"):
+        v = int(file.split('.')[0])
+        file_n = "%05d.png" % v
+        filenames.append(file_n)
+        # os.rename(os.path.join(file), os.path.join(file_n))
+
+    filenames.sort()
+    print(filenames)
+    images = []
+    for filename in filenames:
+        images.append(imageio.imread(filename))
+    imageio.mimsave('anim.gif', images)
+    exit()
 
 
 def images_to_gif(filenames):
+    filenames.sort()
     print(filenames)
     images = []
     for filename in filenames:
@@ -17,33 +45,71 @@ def images_to_gif(filenames):
     imageio.mimsave('anim.gif', images)
 
 
-def draw_frame(lons, lats, altitude, curr_date, dates, id):
-    fig, axs = plt.subplots(2, 1)
-    fig.set_size_inches(10, 15)
+def draw_frame(lons, lats, altitudes, curr_date, dates, id, m, fig, axs):
+    chuncks_lats = []
+    chuncks_lons = []
+    elems_lats = []
+    elems_lons = []
+    cpt = 1
+    for n in range(len(lons)):
+        elems_lats.append(lats[n])
+        elems_lons.append(lons[n])
+
+        if cpt == 2:
+            cpt = 0
+            chuncks_lats.append(elems_lats)
+            elems_lats = []
+            chuncks_lons.append(elems_lons)
+            elems_lons = []
+        cpt += 1
+
+    if len(chuncks_lats) < len(lats):
+        if len(chuncks_lats) == 0:
+            chuncks_lats.append(lats[-1])
+            chuncks_lons.append(lons[-1])
+        else:
+            chuncks_lats.append([chuncks_lats[-1][-1], lats[-1]])
+            chuncks_lons.append([chuncks_lons[-1][-1], lons[-1]])
+
     fig.suptitle(str(curr_date), fontsize=20, x=0.5, y=0.97)
 
+    colors = list(Color("red").range_to(Color("green"), len(chuncks_lats)))
+
+    for i in range(len(chuncks_lats)):
+        x, y = m(chuncks_lons[i], chuncks_lats[i])  # transform coordinates
+        # print(colors[i].rgb)
+        m.plot(x, y, 'o-', markersize=3, linewidth=1, color=colors[i].rgb)
+
+    # fig.suptitle(str(curr_date), fontsize=20, x=0.5, y=0.97)
+    # x, y = m(lons, lats)
+    # m.plot(x, y, 'ro-', markersize=3, linewidth=1)
+    axs[1].set_title('altitude')
+    axs[1].plot(dates, altitudes, 'bo-')
+    #fig.show()
+    path = "%05d.png" % id
+    fig.savefig(path, dpi=100)
+    return path
+
+
+def init_basemap():
+    fig, axs = plt.subplots(2, 1)
+    fig.set_size_inches(10, 15)
     axs[0].set_title("  ")
     m = Basemap(width=1000, height=1000,  projection='merc',
                 ax=axs[0],
-                llcrnrlat=-27.47, urcrnrlat=-27.43,
-                llcrnrlon=29.9, urcrnrlon=29.95,
+                llcrnrlat=-27.46, urcrnrlat=-27.439,
+                llcrnrlon=29.929, urcrnrlon=29.945,
                 lat_ts=1,
                 resolution='f')
     m.drawmapboundary()
     m.drawcoastlines(color='black', linewidth=0.4)
     m.drawrivers(color='blue')
-    m.fillcontinents(color='lightgray')
-    x, y = m(lons, lats)  # transform coordinates
-    m.plot(x, y, 'ro-', markersize=3, linewidth=1)
-
-    axs[1].set_title('altitude')
-    axs[1].plot(dates, altitude)
-    fig.show()
-    fig.savefig(str(id), dpi=100)
-    return "%d.png" % id
+    m.fillcontinents(color='darkgray')
+    return fig, axs, m
 
 
 if __name__ == '__main__':
+    # images_to_gif_()
     f = open("E:\\Kraal\\Kraal Tag Data (GAF54 02 to 04 SEP19) 377rows.csv", 'r')
 
     lats = []
@@ -52,8 +118,10 @@ if __name__ == '__main__':
     dates = []
     image_paths = []
     lines = f.readlines()
+    lines.reverse()
     f.close()
 
+    fig, axs, m = init_basemap()
     for n, line in enumerate(lines):
         if n <= 0:
             continue
@@ -76,7 +144,7 @@ if __name__ == '__main__':
 
             dates.append(date)
 
-            path = draw_frame(lons, lats, altitude, date, dates, n)
+            path = draw_frame(lons, lats, altitude, date, dates, n, m, fig, axs,)
             image_paths.append(path)
         except Exception as e:
             print("Error", e)
--- db_processor/src/data_parser.py
+++ db_processor/src/data_parser.py
@@ -1,7 +1,13 @@
 import time
 import os
 import xlrd
+from datetime import datetime
 from ipython_genutils.py3compat import xrange
+import json
+from sys import exit
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 
 def purge_file(filename):
@@ -22,7 +28,7 @@ def find_data_files(directory_path, extension='.xls'):
     return file_paths
 
 
-def find_files_containing_valid_famacha(book):
+def find_sheet_containing_valid_famacha(book):
     #returns index of famacha column if exists
     map = {}
     for n in range(book.nsheets):
@@ -37,45 +43,59 @@ def find_files_containing_valid_famacha(book):
     return map
 
 
-def process_files(file_paths):
-    valid_files = []
-    for curr_file, path in enumerate(file_paths):
-        try:
-            print("reading file...")
-            print(path)
-            book = xlrd.open_workbook(path)
-            map = find_files_containing_valid_famacha(book)
-            print(map)
-            # if not is_valid:
-            #     continue
-            # valid_files.append(path)
-            # for n in range(book.nsheets):
-            #     sheet = book.sheet_by_index(n)
-            #     found_col_index = False
-            #     for row_index in xrange(0, sheet.nrows):
-            #         try:
-            #             row_values = [sheet.cell(row_index, col_index).value for col_index in xrange(0, sheet.ncols)]
-            #             if "Sender nr" not in row_values:
-            #                 continue
-            #
-            #             famacha_value = row_values[f_col_index]
-            #             id = row_values[row_values.index("Sender nr")]
-            #             date = sheet.name
-            #
-            #             print(row_values)
-            #         except Exception as e:
-            #             print(e)
-        except Exception as e:
-            print(e)
+def format_date(date):
+    split = date.split('-')
+    # print("%s-%s-%s" % (split[0], split[1], split[2][-2:]))
+    return "%s-%s-%s" % (split[0], split[1], split[2][-2:])
 
-    print("found %d valid files out of %d." % (len(valid_files), len(file_paths)) )
+
+def build_famacha_data(map, book, data):
+    for key, value in map.items():
+        sheet = book.sheet_by_name(key)
+        for row_index in xrange(0, sheet.nrows):
+            row_values = [sheet.cell(row_index, col_index).value for col_index in xrange(0, sheet.ncols)]
+            try:
+                id = str(row_values[value['id_col_index']]).split('.')[0]
+                if len(id) < 10:
+                    continue
+                famacha = int(row_values[value['famacha_col_index']])
+                date = datetime.strptime(format_date(key), '%d-%m-%y').strftime('%d/%m/%y')
+            except (TypeError, ValueError) as e:
+                print(e)
+                continue
+            print(id, famacha, date)
+            if id in data and date not in data[id]:
+                data[id][date] = famacha
+            else:
+                data[id] = {date: famacha}
+    return data
+
+
+def process_files(file_paths, farm_name=''):
+    data = {}
+    for curr_file, path in enumerate(file_paths):
+        print("reading file...")
+        print(path)
+        book = xlrd.open_workbook(path)
+        map = find_sheet_containing_valid_famacha(book)
+        if not map:
+            continue
+        print(map)
+        data = build_famacha_data(map, book, data)
+    print('dumping result to json file...')
+    print(data)
+    with open(__location__+'\\%s_famacha_data.json' % farm_name, 'w') as fp:
+        print('')
+        json.dump(data, fp)
 
 
 if __name__ == '__main__':
     print("start...")
     start_time = time.time()
     xls_files = find_data_files("E:/SouthAfrica/Metadata/BOTHAVILLE data", extension='.xls')
-    pdf_files = find_data_files("E:/SouthAfrica/Metadata/BOTHAVILLE data", extension='.pdf')
+    process_files(xls_files, farm_name="bothaville")
 
-    process_files(xls_files)
+    # pdf_files = find_data_files("E:/SouthAfrica/Metadata/BOTHAVILLE data", extension='.pdf')
+
+    exit()
 
