ae690d12f7526e95874ce2edf74a04f56ffb0fa9
fo18103 <Mojjo@2015>
2020-10-05 01:58:40 +0100

    zeros to nan threshold is working. need to save file.

--- db_processor/src/interpolation_thresh.py
+++ db_processor/src/interpolation_thresh.py
@@ -75,7 +75,6 @@ def format_farm_id(farm_id):
     return farm_id
 
 
-
 def thresholded_interpol(df_1min, thresh):
     print("thresholded_interpol...", thresh)
     data = pd.DataFrame(df_1min["first_sensor_value"])
@@ -83,7 +82,7 @@ def thresholded_interpol(df_1min, thresh):
     df = pd.DataFrame(data["first_sensor_value"])
     df['new'] = ((df.notnull() != df.shift().notnull()).cumsum())
     df['ones'] = 1
-    mask["first_sensor_value"] = (df.groupby('new')['ones'].transform('count') < thresh) | data[
+    mask["first_sensor_value"] = (df.groupby('new')['ones'].transform('count') <= thresh) | data[
         "first_sensor_value"].notnull()
     interpolated = data.interpolate().bfill()[mask]
     df_1min["first_sensor_value"] = interpolated
@@ -98,34 +97,69 @@ def thresholded_interpol(df_1min, thresh):
         histogram_array_no_activity_dur.append(zero_gap)
 
     print("thresholded_interpol done.", thresh)
-
-    # df_linterpolated = df_1min.copy()
-    # df_linterpolated['signal_strength_2'] = df_linterpolated['signal_strength']
-    # df_linterpolated['timestamp'] = df_linterpolated.index.values.astype(np.int64) // 10 ** 9
-    # df_linterpolated['date_str'] = df_linterpolated.index.strftime('%Y-%m-%dT%H:%M')
-    # df_linterpolated = df_linterpolated.assign(farm_id=df_linterpolated['farm_id'].max())
-    #
-    # df_linterpolated = df_linterpolated[['timestamp', 'date_str', 'serial_number', 'signal_strength', 'signal_strength_2', 'battery_voltage',
-    #              'first_sensor_value']]
-    #
-    # df_linterpolated = df_linterpolated.assign(serial_number=df_linterpolated['serial_number'].max())  # fills in gap when agg fails because of empty sensor value
-    #
-    # df_linterpolated[['signal_strength', 'signal_strength_2', 'battery_voltage']] = df_linterpolated[['signal_strength', 'signal_strength_2', 'battery_voltage']].fillna(value=0)
-
     return df_1min
+
+
+def zeros_to_nan(df_interpolated, zero_to_nan_threh):
+    print("thresholded_zeros_to_nan...", zero_to_nan_threh)
+    data = pd.DataFrame(df_interpolated["first_sensor_value"])
+    # mask = data.copy()
+    # df = pd.DataFrame(data["first_sensor_value"])
+    # # df['new'] = ((df.notnull() != df.shift().notnull()).cumsum())
+    # # df['ones'] = 1
+    # cpt = 0
+    bzidx = -1
+    ezidx = -1
+    difference = -1
+
+    start_zero_count = False
+    for i, value in enumerate(data["first_sensor_value"]):
+
+        if start_zero_count:
+            difference = i - bzidx
+        if difference >= zero_to_nan_threh:
+            ezidx = i
+            data["first_sensor_value"][bzidx:ezidx] = np.nan
+            #bzidx = i
+            ezidx = -1
+            start_zero_count = False
+            difference = -1
+        if value != 0 and start_zero_count == True:
+            bzidx = -1
+            ezidx = -1
+            start_zero_count = False
+
+        if value == 0 and start_zero_count == False:
+            #if bzidx == -1:
+            bzidx = i
+            start_zero_count = True
+
+    return data
+    #
+    # df['new'] = (((df==0) != (df.shift() == 0)).cumsum())
+    # df['ones'] = 1
+    # group = df.groupby('new')['ones'].transform('count')
+    # mask["first_sensor_value"] = (group >= zero_to_nan_threh)
+    #
+    # dft = pd.DataFrame(df_interpolated["first_sensor_value"])
+    # set_to_nan = dft[dft == mask]
+    #
+    # interpolated = data.interpolate().bfill()[mask]
+    # df_interpolated["first_sensor_value"] = interpolated
+    #return df_interpolated
 
 
 def process_csv(path, zero_to_nan_threh, interpolation_thesh, farm_id, animal_id):
     print("loading data...", path)
     df = pd.read_csv(path, sep=",")
     df_interpolated = thresholded_interpol(df.copy(), interpolation_thesh)
-    export_rawdata_to_csv(df_interpolated, farm_id, animal_id, interpolation_thesh)
+    data_zerofill_thresh = zeros_to_nan(df_interpolated, zero_to_nan_threh)
+    export_rawdata_to_csv(data_zerofill_thresh, farm_id, animal_id, interpolation_thesh, zero_to_nan_threh)
 
 
-
-def export_rawdata_to_csv(df, farm_id, animal_id, thresh):
+def export_rawdata_to_csv(df, farm_id, animal_id, thresh_interpol, thresh_zero2nan):
     print("exporting data...")
-    path = "csv_export/interpolated_1min/%s/interpolation_thesh_%d/" % (farm_id, thresh)
+    path = "csv_export/interpolated_1min/%s/interpolation_thesh_%d_%d/" % (farm_id, thresh_interpol, thresh_zero2nan)
     pathlib.Path(path).mkdir(parents=True, exist_ok=True)
     filename_path = path + "%s.csv" % animal_id
     purge_file(filename_path)
@@ -143,7 +177,7 @@ if __name__ == '__main__':
 
         # csv_file = "C:\\Users\\fo18103\\PycharmProjects\\prediction_of_helminths_infection\\db_processor\\src\\csv_export\\backfill_1min\\delmas_70101200027\\40101310026.csv"
 
-        zero_to_nan_threh = 20
+        zero_to_nan_threh = 5
         interpolation_thesh = 3
 
         if len(sys.argv) > 1:
