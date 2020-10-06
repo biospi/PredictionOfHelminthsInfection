f3d7ea84f3e7dbe80c0ab482897cd84860a57865
fo18103 <Mojjo@2015>
2020-10-06 12:59:45 +0100

    fixed *.csv in file path.

--- pipeline/interpolation_zero2nan_thresh.py
+++ pipeline/interpolation_zero2nan_thresh.py
@@ -126,6 +126,8 @@ def process_csv(output_directory, path, zero_to_nan_threh, interpolation_thesh, 
 
 def export_rawdata_to_csv(output_directory, df, farm_id, animal_id, thresh_interpol, thresh_zero2nan):
     print("exporting data...")
+    pathlib.Path(output_directory).mkdir(parents=True, exist_ok=True)
+    pathlib.Path("%s/%s/" % (output_directory, farm_id)).mkdir(parents=True, exist_ok=True)
     path = "%s/%s/interpolation_thesh_interpol_%d_zeros_%d/" % (output_directory, farm_id, thresh_interpol, thresh_zero2nan)
     print("mkdir", path)
     pathlib.Path(path).mkdir(parents=True, exist_ok=True)
