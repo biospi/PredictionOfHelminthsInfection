142805766adc83cd3950768249e669bda827afa4
fo18103 <Mojjo@2015>
2020-10-07 15:16:34 +0100

    fixed output for each sampling resolution.

--- pipeline/parse_results.py
+++ pipeline/parse_results.py
@@ -24,15 +24,14 @@ if __name__ == "__main__":
         if len(filename) > 45:
             filter_files.append(file)
 
-
     if len(filter_files) == 0:
         print("no final files in %s" % dataset_folder)
         exit(-1)
     print("found %d files" % len(filter_files))
     farm_id = ""
-    df = pd.DataFrame(columns=['usable_11', 'usable_12', 'n_days_before_famacha', 'thresh_interpol', 'thresh_zero'])
+    df = pd.DataFrame(columns=['usable_11', 'usable_12', 'n_days_before_famacha', 'thresh_interpol', 'thresh_zero', 'resolution'])
     for i, file in enumerate(filter_files):
-	print("file=", file)
+        print("file=", file)
         with open(file) as f:
             lines = f.readlines()
             try:
@@ -43,99 +42,102 @@ if __name__ == "__main__":
                 thresh_interpol = int(split[10])
                 thresh_zero = int(split[12].replace(".txt", ""))
                 farm_id = split[4] + "_" + split[5]
+                resolution = file.split('/')[-2].split("_")[2]
             except Exception as e:
-		print("split=", split)
+                print("resolution=", resolution)
+                print("split=", split)
                 print("farm_id=", farm_id)
                 print("usable_11=", usable_11)
                 print("usable_12=", usable_12)
                 print("n_days_before_famacha=", n_days_before_famacha)
                 print("thresh_interpol=", thresh_interpol)
                 print("thresh_zero=", thresh_zero)
-            df.loc[i] = [usable_11, usable_12, n_days_before_famacha, thresh_interpol, thresh_zero]
-
-    # todo reomove
-    df_dummy1 = df.copy()
-    df_dummy1["n_days_before_famacha"] = 5
-    df_dummy_2 = df.copy()
-    df_dummy_2["n_days_before_famacha"] = 2
-    df_dummy = pd.concat([df, df_dummy1, df_dummy_2])
-    df = df_dummy
+                print("resolution=", resolution)
+            df.loc[i] = [usable_11, usable_12, n_days_before_famacha, thresh_interpol, thresh_zero, resolution]
 
     print("DF")
     print(df)
-    list_of_df = [g for _, g in df.groupby(['n_days_before_famacha'])]
+    list_of_df_grouped_by_resolution = [g for _, g in df.groupby(['resolution'])]
 
     try:
         print("mkdir=", output_dir)
         pathlib.Path(output_dir).mkdir(parents=True)
     except Exception as e:
         print(e)
+    try:
+        pathlib.Path(output_dir + "/svg").mkdir(parents=True)
+    except Exception as e:
+        print(e)
+    try:
+        print("mkdir=", output_dir)
+        pathlib.Path(output_dir + "/png").mkdir(parents=True)
+    except Exception as e:
+        print(e)
 
-    fig, axs = plt.subplots(1, len(list_of_df), figsize=(18., 7.2))
-    for i, data_frame in enumerate(list_of_df):
-        data_frame = data_frame.sort_values(by=['thresh_zero'])
-        dbt = int(data_frame["n_days_before_famacha"].values[0])
-        axs[i].plot(data_frame["thresh_zero"], data_frame["usable_11"], label="usable_11")
-        axs[i].plot(data_frame["thresh_zero"], data_frame["usable_12"], label="usable_12")
-        axs[i].set_title("days before test=%d" % dbt)
-        axs[i].set(xlabel='Threshold zeros')
-        axs[i].legend(loc="upper left")
-    # plt.show()
-    out_filename = "%s/thresh_zero_%s_threshi_%d_thresh_z%d.png" % (output_dir, farm_id, thresh_interpol, thresh_zero)
-    fig.savefig(out_filename)
-    fig.savefig(out_filename.replace(".png", ".svg"))
+    # fig, axs = plt.subplots(1, len(list_of_df), figsize=(18., 7.2))
+    # for i, data_frame in enumerate(list_of_df):
+    #     data_frame = data_frame.sort_values(by=['thresh_zero'])
+    #     dbt = int(data_frame["n_days_before_famacha"].values[0])
+    #     resolution = data_frame["resolution"].values[0]
+    #     axs[i].plot(data_frame["thresh_zero"], data_frame["usable_11"], label="usable_11")
+    #     axs[i].plot(data_frame["thresh_zero"], data_frame["usable_12"], label="usable_12")
+    #     axs[i].set_title("sampling=%s days before test=%d" % (resolution, dbt))
+    #     axs[i].set(xlabel='Threshold zeros')
+    #     axs[i].legend(loc="upper left")
+    # # plt.show()
+    # out_filename = "%s/%s_thresh_zero_%s_threshi_%d_thresh_z%d.png" % (resolution, output_dir, farm_id, thresh_interpol, thresh_zero)
+    # fig.savefig(out_filename)
+    # fig.savefig(out_filename.replace(".png", ".svg"))
+    #
+    # plt.close(fig)
+    # plt.clf()
+    # fig, axs = plt.subplots(1, len(list_of_df), figsize=(18., 7.2))
+    # for i, data_frame in enumerate(list_of_df):
+    #     data_frame = data_frame.sort_values(by=['thresh_interpol'])
+    #     dbt = int(data_frame["n_days_before_famacha"].values[0])
+    #     resolution = data_frame["resolution"].values[0]
+    #     axs[i].plot(data_frame["thresh_interpol"], data_frame["usable_11"], label="usable_11")
+    #     axs[i].plot(data_frame["thresh_interpol"], data_frame["usable_12"], label="usable_12")
+    #     axs[i].set_title("sampling=%s days before test=%d" % (resolution, dbt))
+    #     axs[i].set(xlabel='Threshold interpolation')
+    #     axs[i].legend(loc="upper left")
+    # # plt.show()
+    # out_filename = "%s/%s_thresh_interpol_%s_threshi_%d_thresh_z%d.png" % (
+    # output_dir, resolution, farm_id, thresh_interpol, thresh_zero)
+    # fig.savefig(out_filename)
+    # fig.savefig(out_filename.replace(".png", ".svg"))
+    #
+    # plt.close(fig)
+    # plt.clf()
 
-    plt.close(fig)
-    plt.clf()
-    fig, axs = plt.subplots(1, len(list_of_df), figsize=(18., 7.2))
-    for i, data_frame in enumerate(list_of_df):
-        data_frame = data_frame.sort_values(by=['thresh_interpol'])
-        dbt = int(data_frame["n_days_before_famacha"].values[0])
-        axs[i].plot(data_frame["thresh_interpol"], data_frame["usable_11"], label="usable_11")
-        axs[i].plot(data_frame["thresh_interpol"], data_frame["usable_12"], label="usable_12")
-        axs[i].set_title("days before test=%d" % dbt)
-        axs[i].set(xlabel='Threshold interpolation')
-        axs[i].legend(loc="upper left")
-    # plt.show()
-    out_filename = "%s/thresh_interpol_%s_threshi_%d_thresh_z%d.png" % (output_dir, farm_id, thresh_interpol, thresh_zero)
-    fig.savefig(out_filename)
-    fig.savefig(out_filename.replace(".png", ".svg"))
+    for i, df_g_days in enumerate(list_of_df_grouped_by_resolution):
+        grouped_by_days = [g for _, g in df_g_days.groupby(['n_days_before_famacha'])]
+        plt.clf()
+        fig, axs = plt.subplots(2, len(grouped_by_days), figsize=(18., 7.2))
+        for j, df_g_days in enumerate(grouped_by_days):
+            df_g_days = df_g_days.sort_values(by=['thresh_interpol'])
+            dbt = int(df_g_days["n_days_before_famacha"].values[0])
+            resolution = df_g_days["resolution"].values[0]
+            axs[0, j].plot(df_g_days["thresh_interpol"], df_g_days["usable_11"], label="usable_11")
+            axs[0, j].plot(df_g_days["thresh_interpol"], df_g_days["usable_12"], label="usable_12")
+            axs[0, j].set_title("%s %d days window" % (resolution, dbt))
+            axs[0, j].set(xlabel='Threshold interpolation')
+            axs[0, j].legend(loc="upper left")
 
-    plt.close(fig)
-    plt.clf()
+            df_g_days = df_g_days.sort_values(by=['thresh_zero'])
+            dbt = int(df_g_days["n_days_before_famacha"].values[0])
+            axs[1, j].plot(df_g_days["thresh_zero"], df_g_days["usable_11"], label="usable_11")
+            axs[1, j].plot(df_g_days["thresh_zero"], df_g_days["usable_12"], label="usable_12")
+            # axs[1, i].set_title("days before test=%d" % dbt)
+            axs[1, j].set(xlabel='Threshold zeros')
+            axs[1, j].legend(loc="upper left")
 
-    fig, axs = plt.subplots(2, len(list_of_df), figsize=(18., 7.2))
-    for i, data_frame in enumerate(list_of_df):
-        data_frame = data_frame.sort_values(by=['thresh_interpol'])
-        dbt = int(data_frame["n_days_before_famacha"].values[0])
-        axs[0, i].plot(data_frame["thresh_interpol"], data_frame["usable_11"], label="usable_11")
-        axs[0, i].plot(data_frame["thresh_interpol"], data_frame["usable_12"], label="usable_12")
-        axs[0, i].set_title("days before test=%d" % dbt)
-        axs[0, i].set(xlabel='Threshold interpolation')
-        axs[0, i].legend(loc="upper left")
+        out_filename = "%s/png/%s_allthresh_%s_threshi_%d_thresh_z%d.png" % (output_dir, resolution, farm_id, thresh_interpol, thresh_zero)
+        fig.savefig(out_filename)
+        fig.savefig(out_filename.replace(".png", ".svg").replace("png", "svg"))
+        print("final file output=", out_filename)
+        # plt.show()
+        plt.close(fig)
+        plt.clf()
 
-        data_frame = data_frame.sort_values(by=['thresh_zero'])
-        dbt = int(data_frame["n_days_before_famacha"].values[0])
-        axs[1, i].plot(data_frame["thresh_zero"], data_frame["usable_11"], label="usable_11")
-        axs[1, i].plot(data_frame["thresh_zero"], data_frame["usable_12"], label="usable_12")
-        #axs[1, i].set_title("days before test=%d" % dbt)
-        axs[1, i].set(xlabel='Threshold zeros')
-        axs[1, i].legend(loc="upper left")
-
-    # plt.show()
-    plt.close(fig)
-    plt.clf()
-    out_filename = "%s/allthresh_%s_threshi_%d_thresh_z%d.png" % (output_dir, farm_id, thresh_interpol, thresh_zero)
-    fig.savefig(out_filename)
-    fig.savefig(out_filename.replace(".png", ".svg"))
-    print("final file output=", out_filename)
     print("finished!")
-
-
-
-
-
-
-
-
-
