34c32bffc629c00081b950d595563441dc250279
Axel Montout <axel.montout@bristol.ac.uk>
2020-10-08 22:52:20 +0100

    added .sh for export of cedara famacha data to json.

---
+++ pipeline/create_cedara_famacha_json.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ~/Repository/PredictionOfHelminthsInfection/pipeline/cedara_data_parser.py ~/dev_simulation/FamachaMachineLearning/1_h5_files/cedara_famacha_data.json "~/dev_simulation/FamachaMachineLearning/0_raw_data/cedara/famacha_csv/Cedara grafieke.xls" 
