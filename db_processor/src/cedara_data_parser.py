3d5356b48cc7233796e2109083913d7d32527c98
Axel Montout <axel.montout@bristol.ac.uk>
2020-10-08 22:35:34 +0100

    formated cedara famacha report to json script for Linux.

--- db_processor/src/cedara_data_parser.py
+++ db_processor/src/cedara_data_parser.py
@@ -7,28 +7,32 @@ import os
 import json
 from numbers import Number
 from sys import exit
-from ipython_genutils.py3compat import xrange
-pd.set_option('display.max_columns', 48)
-pd.set_option('display.width', 1000)
-pd.set_option('display.max_rows', 1000)
-
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-
-def excel_date_2_datetime(excel_date):
-    return datetime(*xlrd.xldate_as_tuple(excel_date, 0))
+import sys
 
 
 if __name__ == '__main__':
+
+    print("args: output_filename raw_famacha_csv_filename")
+    if len(sys.argv) > 1:
+        output_filename = sys.argv[1]
+        raw_famacha_csv_filename = sys.argv[2]
+    else:
+        exit(-1)
+
+    out_dir = '/'.join(output_filename.split('/')[:-1])
+    print("out_dir=", out_dir)
+    if not os.path.exists(out_dir):
+        print("mkdir", out_dir)
+        os.makedirs(out_dir)
+
     print("start...")
-    path = "E:\\SouthAfrica\\Metadata\\CEDARA data\\Grafieke\\Cedara grafieke.xls"
-    print(path)
+    # raw_famacha_csv_filename = "E:\\SouthAfrica\\Metadata\\CEDARA data\\Grafieke\\Cedara grafieke.xls"
+    print("raw_famacha_csv_filename", raw_famacha_csv_filename)
     data = {}
-    swap_date = datetime(2013, 2, 14, 0, 0)
-    df_list = [pd.read_excel(path, skiprows=range(0, 2), sheet_name='FC tot', header=None),
-               pd.read_excel(path, skiprows=range(0, 2), sheet_name='FC drench', header=None),
-               pd.read_excel(path, skiprows=range(0, 2), sheet_name='FC monitor', header=None)]
+    swap_date = datetime(2013, 2, 14, 0, 0) #todo Sensor swap date check!!!
+    df_list = [pd.read_excel(raw_famacha_csv_filename, skiprows=range(0, 2), sheet_name='FC tot', header=None),
+               pd.read_excel(raw_famacha_csv_filename, skiprows=range(0, 2), sheet_name='FC drench', header=None),
+               pd.read_excel(raw_famacha_csv_filename, skiprows=range(0, 2), sheet_name='FC monitor', header=None)]
     for df in df_list:
         df = df[:-3]
         df = df.drop(df.columns[0], axis=1)
@@ -72,33 +76,14 @@ if __name__ == '__main__':
     print(data)
     print('finished')
     print('dumping result to json file...')
-
     old_keys = data.keys()
     data_formated = {}
     for key in old_keys:
-        new_key = "4001130" + str(key).zfill(4)
+        new_key = "4001130" + str(key).zfill(4) #full id is missing from raw spreadsheet
         for item in data[key]:
             item[2] = new_key
         data_formated[new_key] = data[key]
     print(data_formated)
-    farm_name = "cedara"
-    with open(__location__+'\\%s_famacha_data.json' % farm_name, 'w') as fp:
-        print('')
+    with open(output_filename, 'w') as fp:
         json.dump(data_formated, fp)
-
-
-        # break
-
-    # book = xlrd.open_workbook(path)
-    # for n in range(book.nsheets):
-    #     sheet = book.sheet_by_index(n)
-    #
-    #     if 'FC' not in sheet.name:
-    #         continue
-    #
-    #     for row_index in xrange(0, sheet.nrows):
-    #         try:
-    #             row_values = [sheet.cell(row_index, col_index).value for col_index in xrange(0, sheet.ncols)]
-    #             print(row_values)
-    #         except Exception as e:
-    #             print(e)
+    print(output_filename)
