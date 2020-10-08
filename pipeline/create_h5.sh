ce4ec02dccf9a77b1771c8fedb47f5f8cd329e4b
Axel Montout <axel.montout@bristol.ac.uk>
2020-10-08 22:22:06 +0100

    added .sh for h5 files creation.

---
+++ pipeline/create_h5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo python ~/Repository/PredictionOfHelminthsInfection/pipeline/create_h5_from_csv.py  ~/dev_simulation/FamachaMachineLearning/0_raw_data/delmas/delmas_70101200027.h5 ~/dev_simulation/FamachaMachineLearning/0_raw_data/delmas/raw_csv/
+echo python ~/Repository/PredictionOfHelminthsInfection/pipeline/create_h5_from_csv.py  ~/dev_simulation/FamachaMachineLearning/0_raw_data/ceadra/cedara_70091100056.h5 ~/dev_simulation/FamachaMachineLearning/0_raw_data/cedara/raw_csv/
+
+
