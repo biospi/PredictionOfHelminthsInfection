8d0e288cfbe3bd78612fa6fc18ebbe9255675eb6
Axel Montout <axel.montout@bristol.ac.uk>
2020-10-09 13:21:53 +0100

    fixed threshold sweep bash for test.

--- pipeline/threshold_sweep.sh
+++ pipeline/threshold_sweep.sh
@@ -2,8 +2,8 @@
 #rm -rf /home/axel/dev_simulation/new_pipeline/thresholded/
 njob=24 
 threshz=480
-for threshi in 1; do
-  python interpolation_zero2nan_thresh.py /home/axel/dev_simulation/new_pipeline/thresholded /home/axel/dev_simulation/new_pipeline/backfill_1min/delmas_70101200027/ $threshz $threshi $njob
-done
+threshi=1
+python interpolation_zero2nan_thresh.py /home/axel/dev_simulation/new_pipeline/thresholded /home/axel/dev_simulation/new_pipeline/backfill_1min/delmas_70101200027/ $threshz $threshi $njob
+
 
 
