f89f6aee456f1dc88ee20a10f6643c12f1794831
Axel Montout <axel.montout@bristol.ac.uk>
2020-10-13 13:06:21 +0100

    added parameter to try padding with -1 instead of linear interpol.

--- pipeline/interpolation_zero2nan_thresh.py
+++ pipeline/interpolation_zero2nan_thresh.py
@@ -1,4 +1,5 @@
 import os.path
+import shutil
 from sys import exit
 import glob
 import itertools
@@ -44,7 +45,7 @@ def format_farm_id(farm_id):
     return farm_id
 
 
-def thresholded_interpol(df_1min, thresh):
+def inputation_interpol(df_1min, thresh, force_minus1_padding=False):
     print("thresholded_interpol...", thresh)
     data = pd.DataFrame(df_1min["first_sensor_value"])
     mask = data.copy()
@@ -53,17 +54,20 @@ def thresholded_interpol(df_1min, thresh):
     df['ones'] = 1
     mask["first_sensor_value"] = (df.groupby('new')['ones'].transform('count') <= thresh) | data[
         "first_sensor_value"].notnull()
-    interpolated = data.interpolate().bfill()[mask]
-    df_1min["first_sensor_value"] = interpolated
-    histogram_array_nan_dur , histogram_array_no_activity_dur = [], []
-    # clump = using_clump(data["first_sensor_value"].values)
-    len_holes = [len(list(g)) for k, g in itertools.groupby(data["first_sensor_value"].values, lambda x: np.isnan(x)) if k]
-    for nan_gap in len_holes:
-        histogram_array_nan_dur.append(nan_gap)
+    if force_minus1_padding:
+        padded = data.fillna(-1).bfill()[mask]
+        df_1min["first_sensor_value"] = padded
+    else:
+        interpolated = data.interpolate().bfill()[mask]
+        df_1min["first_sensor_value"] = interpolated
 
-    len_no_activity = [len(list(g)) for k, g in itertools.groupby(data["first_sensor_value"].values, lambda x: x == 0) if k]
-    for zero_gap in len_no_activity:
-        histogram_array_no_activity_dur.append(zero_gap)
+    # len_holes = [len(list(g)) for k, g in itertools.groupby(data["first_sensor_value"].values, lambda x: np.isnan(x)) if k]
+    # for nan_gap in len_holes:
+    #     histogram_array_nan_dur.append(nan_gap)
+    #
+    # len_no_activity = [len(list(g)) for k, g in itertools.groupby(data["first_sensor_value"].values, lambda x: x == 0) if k]
+    # for zero_gap in len_no_activity:
+    #     histogram_array_no_activity_dur.append(zero_gap)
 
     print("thresholded_interpol done.", thresh)
     return df_1min
@@ -83,7 +87,7 @@ def zeros_to_nan(df_interpolated, zero_to_nan_threh):
         if difference >= zero_to_nan_threh:
             ezidx = i
             data["first_sensor_value"][bzidx:ezidx] = np.nan
-            #bzidx = i
+            # bzidx = i
             ezidx = -1
             start_zero_count = False
             difference = -1
@@ -93,7 +97,7 @@ def zeros_to_nan(df_interpolated, zero_to_nan_threh):
             start_zero_count = False
 
         if value == 0 and start_zero_count == False:
-            #if bzidx == -1:
+            # if bzidx == -1:
             bzidx = i
             start_zero_count = True
 
@@ -102,81 +106,98 @@ def zeros_to_nan(df_interpolated, zero_to_nan_threh):
     return df_interpolated_zero2nan
 
 
-def process_csv(output_directory, path, zero_to_nan_threh, interpolation_thesh, farm_id, animal_id):
+def process_csv(output_directory, path, zero_to_nan_threh, interpolation_thesh, farm_id, animal_id,
+                force_minus1_padding):
     print("loading data...", path)
     df = pd.read_csv(path, sep=",")
-    df_interpolated = thresholded_interpol(df.copy(), interpolation_thesh)
+    df_interpolated = inputation_interpol(df.copy(), interpolation_thesh, force_minus1_padding=force_minus1_padding)
     data_zerofill_thresh = zeros_to_nan(df_interpolated, zero_to_nan_threh)
-    export_rawdata_to_csv(output_directory, data_zerofill_thresh, farm_id, animal_id, interpolation_thesh, zero_to_nan_threh)
+    export_rawdata_to_csv(output_directory, data_zerofill_thresh, farm_id, animal_id, interpolation_thesh,
+                          zero_to_nan_threh, force_minus1_padding)
 
 
-def export_rawdata_to_csv(output_directory, df, farm_id, animal_id, thresh_interpol, thresh_zero2nan):
+def create_rec_dir(path):
+    dir_path = ""
+    sub_dirs = path.split("/")
+    for sub_dir in sub_dirs[0:]:
+        dir_path += sub_dir + "/"
+        # print("sub_folder=", dir_path)
+        if not os.path.exists(dir_path):
+            print("mkdir", dir_path)
+            os.makedirs(dir_path)
+
+
+def export_rawdata_to_csv(output_directory, df, farm_id, animal_id, thresh_interpol, thresh_zero2nan,
+                          force_minus1_padding):
     print("exporting data...")
     print("output_directory=", output_directory)
-    try:
-        pathlib.Path(output_directory).mkdir(parents=True)
-    except:
-        pass
+    # try:
+    #     pathlib.Path(output_directory).mkdir(parents=True)
+    # except:
+    #     pass
     print("farm_id=", farm_id)
-    path = "%s/%s/" % (output_directory, farm_id)
-    print("path=",path)
-    try:
-        pathlib.Path(path).mkdir(parents=True)
-    except:
-        pass
-    path = path + "interpol_%d_zeros_%d/" %(thresh_interpol, thresh_zero2nan)
-    print("path2=", path)
-    try:
-        pathlib.Path(path).mkdir()
-        print("created path2")
-    except Exception as e:
-        print(e)
-    filename_path = path + "%s_interpol_%d_zeros_%d.csv" % (animal_id, thresh_interpol, thresh_zero2nan)
-    #print("purge_file")
-    #purge_file(filename_path)
+    path = "%s/%s/force_minus1_padding_%s/interpol_%d_zeros_%d/" % (
+    output_directory, farm_id, force_minus1_padding, thresh_interpol, thresh_zero2nan)
+    # # print("path=",path)
+    # # try:
+    # #     pathlib.Path(path).mkdir(parents=True)
+    # # except:
+    # #     pass
+    # path = path + "interpol_%d_zeros_%d_%s/" %(thresh_interpol, thresh_zero2nan, farm_id)
+    # # print("path2=", path)
+    # # try:
+    # #     pathlib.Path(path).mkdir()
+    # #     print("created path2")
+    # # except Exception as e:
+    # #     print(e)
+    create_rec_dir(path)
+    filename_path = path + "%s_interpol_%d_zeros_%d_farmid_%s_forcemop_%s.csv" % (
+    animal_id, thresh_interpol, thresh_zero2nan, farm_id, force_minus1_padding)
+    # print("purge_file")
+    # purge_file(filename_path)
     print("create_file=", filename_path)
     df.to_csv(filename_path, sep=',', index=False)
     print("final file=", filename_path)
 
 
-if __name__ == '__main__':
-    print("start...")
-    # __location__ = os.path.realpath(
-    #     os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    # print(__location__)
+def parse_filename(file):
+    farm_id = file.split('/')[-2]
+    animal_id = file.split('/')[-1].replace('.csv', '')
+    return farm_id, animal_id
 
-    # csv_dir_path = "C:\\Users\\fo18103\\PycharmProjects\\prediction_of_helminths_infection\\db_processor\\src\\csv_export\\backfill_1min\\delmas_70101200027\\*.csv"
-    # zero_to_nan_threh = 60*4
-    # interpolation_thesh = 60
-    # n_process = 6
-    print("arg: output_dir csv_dir_path zero_to_nan_threh interpolation_thesh n_process")
+
+if __name__ == '__main__':
+    print("arg: output_dir csv_dir_path interpolation_thesh zero_to_nan_threh force_minus1_padding n_process")
     if len(sys.argv) > 1:
-        output_directory = sys.argv[1]
+        output_dir = sys.argv[1]
         csv_dir_path = sys.argv[2]
-        zero_to_nan_threh = sys.argv[3]
-        interpolation_thesh = sys.argv[4]
-        n_process = int(sys.argv[5])
+        interpolation_thesh = sys.argv[3]
+        zero_to_nan_threh = sys.argv[4]
+        force_minus1_padding = sys.argv[5].lower() == 'true'
+        n_process = int(sys.argv[6])
     else:
         exit(-1)
 
+
     files = glob.glob(csv_dir_path + "/*.csv")
     print("found %d files." % len(files))
-    #files = files[0:1]
+
     MULTI_THREADING_ENABLED = (n_process > 1)
+    print("MULTI_THREADING_ENABLED=", MULTI_THREADING_ENABLED)
 
     if MULTI_THREADING_ENABLED:
         pool = Pool(processes=n_process)
         for idx, csv_file in enumerate(files):
             csv_file = csv_file.replace("\\", "/")
-	    print(csv_file)
-            farm_id = csv_file.split('/')[-2]
-            animal_id = csv_file.split('/')[-1].replace('.csv', '')
-            pool.apply_async(process_csv, (output_directory, csv_file, int(zero_to_nan_threh), int(interpolation_thesh), farm_id, animal_id,))
+            farm_id, animal_id = parse_filename(csv_file)
+            pool.apply_async(process_csv, (
+            output_dir, csv_file, int(zero_to_nan_threh), int(interpolation_thesh), farm_id, animal_id,
+            force_minus1_padding,))
         pool.close()
         pool.join()
         pool.terminate()
     else:
         for csv_file in files:
-            farm_id = csv_file.split('/')[-2]
-            animal_id = csv_file.split('/')[-1].replace('.csv','')
-            process_csv(output_directory, csv_file, int(zero_to_nan_threh), int(interpolation_thesh), farm_id, animal_id)
+            farm_id, animal_id = parse_filename(csv_file)
+            process_csv(output_dir, csv_file, int(zero_to_nan_threh), int(interpolation_thesh), farm_id, animal_id,
+                        force_minus1_padding)
