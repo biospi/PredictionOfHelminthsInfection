52a27fd9c67eb11335ff1996725891828f910c8f
Ranjeet Bhamber <ranjeet@bhamber.com>
2020-10-16 21:25:15 +0100

    .

--- commands/Samples.py
+++ commands/Samples.py
@@ -20,77 +20,77 @@
 #
 
 import numpy as np
- import pandas as pd
- from enum import Enum, auto
- from typing import Final
- from pathlib import Path
- import datetime as dt
+import pandas as pd
+from enum import Enum, auto
+from typing import Final
+from pathlib import Path
+import datetime as dt
 
- class TimeConst:
-     utc1min: Final = 60
-     utc1hour: Final = 60*utc1min
-     utc1day: Final = 24*utc1hour
+class TimeConst:
+    utc1min: Final = 60
+    utc1hour: Final = 60*utc1min
+    utc1day: Final = 24*utc1hour
 
- class Score(Enum):
-     f1 = auto()
-     f2 = auto()
-     f3 = auto()
-     f4 = auto()
-     f5 = auto()
-     f1To1 = auto()
-     f1To2 = auto()
-     f1To3 = auto()
-     f2To2 = auto()
-     f2To1 = auto()
-     f2To3 = auto()
-     f3To3 = auto()
-     f3To2 = auto()
-     f3To1 = auto()
+class Score(Enum):
+    f1 = auto()
+    f2 = auto()
+    f3 = auto()
+    f4 = auto()
+    f5 = auto()
+    f1To1 = auto()
+    f1To2 = auto()
+    f1To3 = auto()
+    f2To2 = auto()
+    f2To1 = auto()
+    f2To3 = auto()
+    f3To3 = auto()
+    f3To2 = auto()
+    f3To1 = auto()
 
- class Activity:
-     def __init__(self, _id, _trace, _time):
-         self.ID = _id
-         self.A = _trace
-         self.T = _time
-     def getTime(self):
-         return np.array([dt.datetime.utcfromtimestamp(x) for x in  self.T])
-     def getUTC(self):
-         return self.T
-     def getTrace(self):
-         return self.A
-     ID = [] # ID of Animal
-     T = []  # Time of each activity measurement
-     A = []  # Activity trace of animal
+class Activity:
+    def __init__(self, _id, _trace, _time):
+        self.ID = _id
+        self.A = _trace
+        self.T = _time
+    def getTime(self):
+        return np.array([dt.datetime.utcfromtimestamp(x) for x in  self.T])
+    def getUTC(self):
+        return self.T
+    def getTrace(self):
+        return self.A
+    ID = [] # ID of Animal
+    T = []  # Time of each activity measurement
+    A = []  # Activity trace of animal
 
- class Sample:
-     ID = 0
-     date = 0
-     activity = 0
-     famacha = 0
-     deltaFamacha = 0
+class Sample:
+    ID = 0
+    date = 0
+    activity = 0
+    famacha = 0
+    deltaFamacha = 0
 
- def loadActivityTrace(dirName):
-     """
-     Load in data from directory containing csv files containing activity trace data return list of Activity
-     Trace objects.
-     """
-     files = Path(dirName)
-     fileName = [ x.stem[0:x.stem.find('_')] for x in files]
+def loadActivityTrace(dirName):
+    """
+    Load in data from directory containing csv files containing activity trace data return list of Activity
+    Trace objects.
+    """
+    files = Path(dirName)
+    fileName = [ x.stem[0:x.stem.find('_')] for x in files]
 
-     animalTrace = []
+    animalTrace = []
 
-     # Get rid of any median file, this will be unnecessary in the future
-     if 'median' in fileName:
-         idx = fileName.index('median')
-         del files[idx]
-         del fileName[idx]
+    # Get rid of any median file, this will be unnecessary in the future
+    if 'median' in fileName:
+        idx = fileName.index('median')
+        del files[idx]
+        del fileName[idx]
 
-     for i, idx in enumerate(files):
-         dataFrame = pd.read_csv(i)
-         atrace = np.array(dataFrame.loc[:,'first_sensor_value'])
-         atime = np.array(dataFrame.loc[:,'timestamp'])
-         animalTrace.append(Activity(fileName[idx], atrace, atime))
-     return animalTrace
+    for i, idx in enumerate(files):
+        dataFrame = pd.read_csv(i)
+        atrace = np.array(dataFrame.loc[:,'first_sensor_value'])
+        atime = np.array(dataFrame.loc[:,'timestamp'])
+        animalTrace.append(Activity(fileName[idx], atrace, atime))
+    return animalTrace
 
 
 #%%
