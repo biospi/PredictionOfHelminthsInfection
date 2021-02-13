fixed rmse calculation error. fixed missingness mask.

gainimputation/helper.py
@@ -15,7 +15,6 @@ import numpy as np
 np.random.seed(0) #for reproducability
 #import tensorflow as tf
 import tensorflow.compat.v1 as tf
-import warnings
 
 tf.disable_v2_behavior()
 
@@ -139,23 +138,34 @@ def rmse_loss(ori_data, imputed_data, data_m):
   '''
 
   ori_data[np.isnan(ori_data)] = 0 #ignore real nan
+  #
+  # ori_data, norm_parameters = normalization(ori_data)
+  # imputed_data, _ = normalization(imputed_data, norm_parameters)
+  #
+  # # Only for missing values
+  # A = (data_m) * ori_data
+  # B = (data_m) * imputed_data
+  # C = (A - B)
+  # nominator = np.sum(C**2)
+  # denominator = np.sum(data_m)
+  #
+  # print("nominator=", nominator)
+  # print("denominator=", denominator)
+  # rmse = np.sqrt(nominator/float(denominator))
 
   ori_data, norm_parameters = normalization(ori_data)
   imputed_data, _ = normalization(imputed_data, norm_parameters)
-    
-  # Only for missing values
-  A = (data_m) * ori_data
-  B = (data_m) * imputed_data
-  C = (A - B)
-  nominator = np.sum(C**2)
-  denominator = np.sum(data_m)
 
-  print("nominator=", nominator)
-  print("denominator=", denominator)
-  rmse = np.sqrt(nominator/float(denominator))
+  # Only for missing values
+  A = (1 - data_m) * ori_data
+  B = (1 - data_m) * imputed_data
+  nominator = np.sum((A - B) ** 2)
+  denominator = np.sum(1 - data_m)
+
+  rmse = np.sqrt(nominator / float(denominator))
 
   if np.isnan(rmse):
-    warnings.warn("Error while calculating RMSE is NaN")
+    raise ValueError("Error while calculating RMSE is NaN")
 
   return rmse
 

imputation.py
@@ -288,8 +288,13 @@ def process(data_x, miss_rate):
         # Introduce missing data
         data_m = binary_sampler(1 - miss_rate, no, dim)
         miss_data_x = data_x.copy()
+        miss_data_x[np.isnan(miss_data_x)] = -99
+        miss_data_x[data_m == 0] = np.nan
+        miss_data_x[np.isnan(data_x)] = -99
+        data_m = np.ones((no, dim), dtype=int)
         data_m[np.isnan(miss_data_x)] = 0
-        miss_data_x[data_m == 1] = np.nan
+        data_m[miss_data_x == -99] = 1
+        miss_data_x[miss_data_x == -99] = np.nan
 
     return data_x, miss_data_x, data_m
 