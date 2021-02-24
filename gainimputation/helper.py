fixed rmse calculation.

gainimputation/helper.py
@@ -16,6 +16,7 @@ np.random.seed(0) #for reproducability
 #import tensorflow as tf
 import tensorflow.compat.v1 as tf
 from utils.Utils import anscombe
+import warnings
 
 tf.disable_v2_behavior()
 
@@ -143,20 +144,29 @@ def rmse_loss(ori_data, imputed_data, data_m):
   ori_data_ = ori_data.copy()
   ori_data[ori_data_ == np.log(anscombe(0))] = np.nan
   imputed_data[ori_data_ == np.log(anscombe(0))] = np.nan
-  ori_data[ori_data_ == 0] = np.nan
-  imputed_data[ori_data_ == 0] = np.nan
 
-  ori_data_norm, norm_parameters = normalization(ori_data)
-  imputed_data_norm, _ = normalization(imputed_data, norm_parameters)
+  # ori_data_norm, norm_parameters = normalization(ori_data)
+  # imputed_data_norm, _ = normalization(imputed_data, norm_parameters)
+
+  ori_data[np.isnan(ori_data)] = 0
+  imputed_data[np.isnan(imputed_data)] = 0
 
   # Only for missing values
   A = (1 - data_m) * ori_data
   B = (1 - data_m) * imputed_data
 
-  A[A == 0] = np.nan
-  B[B == 0] = np.nan
+  if A[(B == 0) & (A > 0)].size > 0:
+    warnings.warn("erroneous point! nan or zeros in imputation results")
+    A[(B == 0) & (A > 0)] = 0
 
-  if A[~np.isnan(A)].size != B[~np.isnan(B)].size:
+  if A[(A == 0) & (B > 0)].size > 0:
+    raise ValueError("erroneous point!")
+
+  a = A[A>0].size
+  b = B[B>0].size
+
+  if a != b:
+      print(a, b)
       raise ValueError("should have same number of point for rmse calculation!")
 
   C = A - B
@@ -164,8 +174,8 @@ def rmse_loss(ori_data, imputed_data, data_m):
   if C[~np.isnan(C)].shape[0] == 0:
       raise ValueError("empty array!")
 
-  nominator = np.nansum(C ** 2)
-  denominator = np.nansum(1 - data_m)
+  nominator = np.sum(C ** 2)
+  denominator = np.sum(1 - data_m)
 
   print("nominator=", nominator)
   print("denominator=", denominator)

imputation_test.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
         raw_data, original_data_x, ids, timestamp, date_str = imputation.load_farm_data(DATA_DIR, NJOB, NTOP, enable_remove_zeros=REMOVE_ZEROS,
                                                                                         enable_anscombe=ANSCOMBE, enable_log_anscombe=LOG_ANSCOMBE, window=WINDOW_ON)
         iteration_range = np.array(list(range(10, I_RANGE, 10)))
-        missing_range = [0.01]
+        missing_range = [0.02]
         if ENABLE_FINAL_IMP:
             missing_range = [0]
             iteration_range = [600]