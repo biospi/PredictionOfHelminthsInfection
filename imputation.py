updated pipeline, back transform data to count after imputation, fixed quotient normalisation? , created Anscombe object for make_pipeline.

imputation.py
@@ -25,7 +25,7 @@ from data_imputation.gain import gain
 from data_imputation.helper import binary_sampler, linear_interpolation_v, reshape_matrix_andy, reshape_matrix_ranjeet, \
     restore_matrix_andy, build_formated_axis, linear_interpolation_h
 from data_imputation.helper import rmse_loss
-from utils.Utils import create_rec_dir
+from utils.Utils import create_rec_dir, inverse_anscombe
 import matplotlib.pyplot as plt
 import matplotlib
 #matplotlib.use('Qt5Agg')
@@ -182,14 +182,15 @@ def export_imputed_data(out, data_m_x, ori_data_x, idata, ildata, timestamp, dat
     print("exporting imputed data...")
     print(ids)
     data_m_x[np.isnan(data_m_x)] = 1
+
     for i in range(idata.shape[1]):
         print("progress %d/%d ..." % (i, len(ids)))
         df = pd.DataFrame()
         df["timestamp"] = timestamp.values
         df["date_str"] = date_str.values
-        df["first_sensor_value"] = ori_data_x[:, i]
-        df["first_sensor_value_gain"] = idata[:, i]
-        df["first_sensor_value_li"] = ildata[:, i]
+        df["first_sensor_value"] = np.array([x if np.isnan(x) else int(x) for x in inverse_anscombe(np.exp(ori_data_x[:, i]), 0)])
+        df["first_sensor_value_gain"] = np.array([x if np.isnan(x) else int(x) for x in inverse_anscombe(np.exp(idata[:, i]), 0)])
+        df["first_sensor_value_li"] = np.array([x if np.isnan(x) else int(x) for x in inverse_anscombe(np.exp(ildata[:, i]), 0)])
         df["imputed"] = (idata[:, i] > 0).astype(int)
         id = str(ids[i])
         filename = id + ".csv"
@@ -245,23 +246,23 @@ def load_farm_data(fname, n_job, n_top_traces=0, enable_anscombe=False, enable_l
         if enable_remove_zeros:
             activity = a_data[1]["first_sensor_value"].replace(0, np.nan)
             #activity[activity < 2] = np.nan
-        activity_o = activity
+        activity_o = activity.values
         power1_o = power1
         power2_o = power2
 
         if enable_anscombe:
-            anscombe_m = np.vectorize(anscombe)
             # activity = anscombe_m(np.log(activity, out=np.zeros_like(activity), where=(activity != 0)))
             # activity = np.log(activity, out=np.zeros_like(activity), where=(activity != 0))
-            activity = anscombe_m(activity)
-            power1 = anscombe_m(power1)
-            power2 = anscombe_m(power2)
+            activity = anscombe(activity)
 
         if enable_log_anscombe:
-            anscombe_m = np.vectorize(anscombe)
-            activity = np.log(anscombe_m(activity))
-            power1 = np.log(anscombe_m(power1))
-            power2 = np.log(anscombe_m(power2))
+            activity = np.log(anscombe(activity.values, 0))
+            activity_reverse = np.array([x if np.isnan(x) else int(x) for x in inverse_anscombe(np.exp(activity), 0)])#cast to count
+
+            assert np.array_equal(activity_o, activity_reverse, equal_nan=True), "Inverse ascombe failed!"
+
+            # power1 = np.log(anscombe_m(power1))
+            # power2 = np.log(anscombe_m(power2))
 
         data_first_sensor[a_data[0]] = [e] + activity.tolist()
 

ml_pipeline.py
@@ -11,6 +11,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
 
+from utils._anscombe import Anscombe
 from utils._custom_split import StratifiedLeaveTwoOut
 from utils._normalisation import QuotientNormalizer
 
@@ -1189,11 +1190,9 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
         report_rows_list.append(scores)
         del scores
 
-        print('->StandardScaler->MLP' + str(mlp_layers))
-        clf_std_mlp = make_pipeline(preprocessing.StandardScaler(with_mean=True, with_std=False),
-                                    MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu',
-                                                  solver='adam', random_state=0))
-        scores = cross_validate(clf_std_mlp, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
+        print('->StandardScaler->Anscombe->SVC')
+        clf_std_svc = make_pipeline(preprocessing.StandardScaler(with_mean=True, with_std=False), Anscombe(), SVC(probability=True, class_weight='balanced'))
+        scores = cross_validate(clf_std_svc, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
         scores["downsample"] = downsample_false_class
         scores["class0"] = y[y == class_healthy].size
         scores["class1"] = y[y == class_unhealthy].size
@@ -1208,15 +1207,42 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
         scores["f1_score0_mean"] = np.mean(scores["test_f1_score0"])
         scores["f1_score1_mean"] = np.mean(scores["test_f1_score1"])
         scores["sampling"] = sampling
-        scores["classifier"] = "->StandardScaler->MLP" + str(mlp_layers)
-        scores["classifier_details"] = str(clf_std_mlp).replace('\n', '').replace(" ", '')
-        clf_std_mlp = make_pipeline(preprocessing.StandardScaler(with_mean=True, with_std=False),
-                                    MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu',
-                                                  solver='adam', random_state=0))
-        aucs = make_roc_curve(out_dir, clf_std_mlp, X.copy(), y.copy(), cross_validation_method, param_str)
+        scores["classifier"] = "->StandardScaler->Anscombe->SVC"
+        scores["classifier_details"] = str(clf_std_svc).replace('\n', '').replace(" ", '')
+        clf_std_svc = make_pipeline(preprocessing.StandardScaler(with_mean=True, with_std=False), Anscombe(), SVC(probability=True, class_weight='balanced'))
+        aucs = make_roc_curve(out_dir, clf_std_svc, X.copy(), y.copy(), cross_validation_method, param_str)
         scores["roc_auc_score_mean"] = aucs
         report_rows_list.append(scores)
         del scores
+
+        # print('->StandardScaler->MLP' + str(mlp_layers))
+        # clf_std_mlp = make_pipeline(preprocessing.StandardScaler(with_mean=True, with_std=False),
+        #                             MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu',
+        #                                           solver='adam', random_state=0))
+        # scores = cross_validate(clf_std_mlp, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
+        # scores["downsample"] = downsample_false_class
+        # scores["class0"] = y[y == class_healthy].size
+        # scores["class1"] = y[y == class_unhealthy].size
+        # scores["option"] = option
+        # scores["days"] = days
+        # scores["farm_id"] = farm_id
+        # scores["balanced_accuracy_score_mean"] = np.mean(scores["test_balanced_accuracy_score"])
+        # scores["precision_score0_mean"] = np.mean(scores["test_precision_score0"])
+        # scores["precision_score1_mean"] = np.mean(scores["test_precision_score1"])
+        # scores["recall_score0_mean"] = np.mean(scores["test_recall_score0"])
+        # scores["recall_score1_mean"] = np.mean(scores["test_recall_score1"])
+        # scores["f1_score0_mean"] = np.mean(scores["test_f1_score0"])
+        # scores["f1_score1_mean"] = np.mean(scores["test_f1_score1"])
+        # scores["sampling"] = sampling
+        # scores["classifier"] = "->StandardScaler->MLP" + str(mlp_layers)
+        # scores["classifier_details"] = str(clf_std_mlp).replace('\n', '').replace(" ", '')
+        # clf_std_mlp = make_pipeline(preprocessing.StandardScaler(with_mean=True, with_std=False),
+        #                             MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu',
+        #                                           solver='adam', random_state=0))
+        # aucs = make_roc_curve(out_dir, clf_std_mlp, X.copy(), y.copy(), cross_validation_method, param_str)
+        # scores["roc_auc_score_mean"] = aucs
+        # report_rows_list.append(scores)
+        # del scores
 
     print('->SVC')
     clf_svc = make_pipeline(SVC(probability=True, class_weight='balanced'))
@@ -1243,10 +1269,9 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
     report_rows_list.append(scores)
     del scores
 
-    print('->MLP' + str(mlp_layers))
-    clf_mlp = make_pipeline(
-        MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu', solver='adam', random_state=0))
-    scores = cross_validate(clf_mlp, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
+    print('Anscombe->SVC')
+    clf_svc = make_pipeline(Anscombe(), SVC(probability=True, class_weight='balanced'))
+    scores = cross_validate(clf_svc, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
     scores["downsample"] = downsample_false_class
     scores["class0"] = y[y == class_healthy].size
     scores["class1"] = y[y == class_unhealthy].size
@@ -1261,14 +1286,40 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
     scores["f1_score0_mean"] = np.mean(scores["test_f1_score0"])
     scores["f1_score1_mean"] = np.mean(scores["test_f1_score1"])
     scores["sampling"] = sampling
-    scores["classifier"] = "->MLP" + str(mlp_layers)
-    scores["classifier_details"] = str(clf_mlp).replace('\n', '').replace(" ", '')
-    clf_mlp = make_pipeline(
-        MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu', solver='adam', random_state=0))
-    aucs = make_roc_curve(out_dir, clf_mlp, X.copy(), y.copy(), cross_validation_method, param_str)
+    scores["classifier"] = "Anscombe->SVC"
+    scores["classifier_details"] = str(clf_svc).replace('\n', '').replace(" ", '')
+    clf_svc = make_pipeline(Anscombe(), SVC(probability=True, class_weight='balanced'))
+    aucs = make_roc_curve(out_dir, clf_svc, X.copy(), y.copy(), cross_validation_method, param_str)
     scores["roc_auc_score_mean"] = aucs
     report_rows_list.append(scores)
     del scores
+
+    # print('->MLP' + str(mlp_layers))
+    # clf_mlp = make_pipeline(
+    #     MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu', solver='adam', random_state=0))
+    # scores = cross_validate(clf_mlp, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
+    # scores["downsample"] = downsample_false_class
+    # scores["class0"] = y[y == class_healthy].size
+    # scores["class1"] = y[y == class_unhealthy].size
+    # scores["option"] = option
+    # scores["days"] = days
+    # scores["farm_id"] = farm_id
+    # scores["balanced_accuracy_score_mean"] = np.mean(scores["test_balanced_accuracy_score"])
+    # scores["precision_score0_mean"] = np.mean(scores["test_precision_score0"])
+    # scores["precision_score1_mean"] = np.mean(scores["test_precision_score1"])
+    # scores["recall_score0_mean"] = np.mean(scores["test_recall_score0"])
+    # scores["recall_score1_mean"] = np.mean(scores["test_recall_score1"])
+    # scores["f1_score0_mean"] = np.mean(scores["test_f1_score0"])
+    # scores["f1_score1_mean"] = np.mean(scores["test_f1_score1"])
+    # scores["sampling"] = sampling
+    # scores["classifier"] = "->MLP" + str(mlp_layers)
+    # scores["classifier_details"] = str(clf_mlp).replace('\n', '').replace(" ", '')
+    # clf_mlp = make_pipeline(
+    #     MLPClassifier(hidden_layer_sizes=mlp_layers, max_iter=300, activation='relu', solver='adam', random_state=0))
+    # aucs = make_roc_curve(out_dir, clf_mlp, X.copy(), y.copy(), cross_validation_method, param_str)
+    # scores["roc_auc_score_mean"] = aucs
+    # report_rows_list.append(scores)
+    # del scores
 
     df_report = pd.DataFrame(report_rows_list)
     df_report["class_0_label"] = label_series[class_healthy]
@@ -1287,8 +1338,6 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
         os.makedirs(output_dir)
     df_report.to_csv(filename, sep=',', index=False)
     print("filename=", filename)
-
-
 
 
 def get_proba(y_probas, y_pred):

utils/Utils.py
@@ -1,15 +1,87 @@
 import math
 import os
+import numpy as np
 
 """
 Utility class for static methods
 """
 
-def anscombe(value):
-    try:
-        return 2 * math.sqrt(value + (3 / 8))
-    except TypeError as e:
-        print(e)
+# def anscombe_r(value):
+#     try:
+#         return (value*value) / 2
+#     except TypeError as e:
+#         print(e)
+#
+# def anscombe(value):
+#     try:
+#         return 2 * math.sqrt(value + (3 / 8))
+#     except TypeError as e:
+#         print(e)
+
+def anscombe(arr, sigma_sq=0, alpha=1):
+    """
+    Generalized Anscombe variance-stabilizing transformation
+    References:
+    [1] http://www.cs.tut.fi/~foi/invansc/
+    [2] M. Makitalo and A. Foi, "Optimal inversion of the generalized
+    Anscombe transformation for Poisson-Gaussian noise", IEEE Trans.
+    Image Process, 2012
+    [3] J.L. Starck, F. Murtagh, and A. Bijaoui, Image  Processing
+    and Data Analysis, Cambridge University Press, Cambridge, 1998)
+    :param arr: variance-stabilized signal
+    :param sigma_sq: variance of the Gaussian noise component
+    :param alpha: scaling factor of the Poisson noise component
+    :return: variance-stabilized array
+    """
+    v = np.maximum((arr / alpha) + (3. / 8.) + sigma_sq / (alpha ** 2), 0)
+    f = 2. * np.sqrt(v)
+    return f
+
+
+def inverse_anscombe(arr, sigma_sq=0, m=0, alpha=1, method='closed-form'):
+    """
+    Inverse of the Generalized Anscombe variance-stabilizing
+    transformation
+    References:
+    [1] http://www.cs.tut.fi/~foi/invansc/
+    [2] M. Makitalo and A. Foi, "Optimal inversion of the generalized
+    Anscombe transformation for Poisson-Gaussian noise", IEEE Trans.
+    Image Process, 2012
+    [3] J.L. Starck, F. Murtagh, and A. Bijaoui, Image  Processing
+    and Data Analysis, Cambridge University Press, Cambridge, 1998)
+
+
+    :param arr: variance-stabilized signal
+    :param sigma_sq: variance of the Gaussian noise component
+    :param m: mean of the Gaussian noise component
+    :param alpha: scaling factor of the Poisson noise component
+    :param method: 'closed_form' applies the closed-form approximation
+    of the exact unbiased inverse. 'asym' applies the asymptotic
+    approximation of the exact unbiased inverse.
+    :return: inverse variance-stabilized array
+    """
+    sigma_sq /= alpha ** 2
+
+    if method == 'closed-form':
+        # closed-form approximation of the exact unbiased inverse:
+        arr_trunc = np.maximum(arr, 0.8)
+        inverse = ((arr_trunc / 2.) ** 2 + 0.25 * np.sqrt(1.5) * arr_trunc ** -1 - (11. / 8.) * arr_trunc ** -2 +
+                   (5. / 8.) * np.sqrt(1.5) * arr_trunc ** -3 - (1. / 8.) - sigma_sq)
+    elif method == 'asym':
+        # asymptotic approximation of the exact unbiased inverse:
+        inverse = (arr / 2.) ** 2 - 1. / 8 - sigma_sq
+        # inverse = np.maximum(0, inverse)
+    else:
+        raise NotImplementedError('Only supports the closed-form')
+
+    if alpha != 1:
+        inverse *= alpha
+
+    if m != 0:
+        inverse += m
+
+    return inverse
+
 
 def create_rec_dir(path):
     dir_path = ""

utils/_anscombe.py
@@ -8,50 +8,39 @@ from sklearn.preprocessing import Normalizer
 from sklearn.utils import check_array
 import numpy as np
 
+from utils.Utils import inverse_anscombe
+
 np.random.seed(0)
 
-
-def normalize(X):
-    X = X.astype(np.float)
-    #step 1 find pointwise median sample [median of col1, .... median of col n].
-    median_array = np.median(X, axis=0)
-
-    #step 2 divide each sample by median array
-    X_median = []
-    for x in X:
-        div = np.divide(x, median_array, out=np.zeros_like(x), where=median_array != 0) #return 0 if div by 0!
-        X_median.append(div)
-
-    #step 3 Within each sample (from iii) store the median value of the sample, which will produce an array of
-    # median values (1 per samples).
-    within_median = []
-    for msample in X_median:
-        within_median.append(np.median(msample))
-
-    #step 4 Use the array of medians to scale(multiply) each original sample, which will give all quotient normalized samples.
-    qnorm_sample = []
-    for i, s in enumerate(X):
-        qnorm_sample.append(s * within_median[i])
-
-    #step 5 Multiply each quotient normalised sample by the total sum off all original samples divided by the sum of
-    # f all element in the original corresponding sample.
-    T = np.sum(X.flatten())
-    t = []
-    for orig_sample in X:
-        t.append(np.sum(orig_sample))
-
-    qnorm_sample_ = []
-    for i, qqsample in enumerate(qnorm_sample):
-        qnorm_sample_.append(qqsample * T/t[i])
-
-    df_norm = np.array(qnorm_sample_)
-    return df_norm
+def anscombe(arr, sigma_sq=0, alpha=1):
+    """
+    Generalized Anscombe variance-stabilizing transformation
+    References:
+    [1] http://www.cs.tut.fi/~foi/invansc/
+    [2] M. Makitalo and A. Foi, "Optimal inversion of the generalized
+    Anscombe transformation for Poisson-Gaussian noise", IEEE Trans.
+    Image Process, 2012
+    [3] J.L. Starck, F. Murtagh, and A. Bijaoui, Image  Processing
+    and Data Analysis, Cambridge University Press, Cambridge, 1998)
+    :param arr: variance-stabilized signal
+    :param sigma_sq: variance of the Gaussian noise component
+    :param alpha: scaling factor of the Poisson noise component
+    :return: variance-stabilized array
+    """
+    v = np.maximum((arr / alpha) + (3. / 8.) + sigma_sq / (alpha ** 2), 0)
+    f = 2. * np.sqrt(v)
+    return f
 
 
-class QuotientNormalizer(TransformerMixin, BaseEstimator):
-    def __init__(self, norm='q', *, copy=True):
-        self.norm = norm
+class Anscombe(TransformerMixin, BaseEstimator):
+    def __init__(self, *, copy=True):
         self.copy = copy
+
+    def _reset(self):
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+        __init__ parameters are not touched.
+        """
 
     def fit(self, X, y=None):
         """Do nothing and return the estimator unchanged
@@ -66,20 +55,15 @@ class QuotientNormalizer(TransformerMixin, BaseEstimator):
         self._validate_data(X, accept_sparse='csr')
         return self
 
-    def transform(self, X, copy=None):
-        """Scale each non zero row of X to unit norm
+    def partial_fit(self, X, y=None):
+        return self
 
-        Parameters
-        ----------
-        X : {array-like, sparse matrix}, shape [n_samples, n_features]
-            The data to normalize, row by row. scipy.sparse matrices should be
-            in CSR format to avoid an un-necessary copy.
-        copy : bool, optional (default: None)
-            Copy the input X or not.
-        """
-        #copy = copy if copy is not None else self.copy
+    def transform(self, X, copy=None):
         X = check_array(X, accept_sparse='csr')
-        return normalize(X)
+        return anscombe(X)
+
+    def inverse_transform(self, X):
+        return inverse_anscombe(X)
 
 
 def createSynthetic(activity):
@@ -102,7 +86,7 @@ def plotData(X, title="Activity sample before quotient normalisation"):
             x=timestamp,
             y=sample,
         ), row=1, col=1)
-    fig.update_layout(yaxis_range=[0, 40])
+    fig.update_layout(yaxis_range=[0, 20])
     fig.update_layout(title_text=title)
     fig.show()
 
@@ -131,7 +115,7 @@ if __name__ == "__main__":
     X = df.values
     print("X=", X)
 
-    X_normalized = QuotientNormalizer().transform(X)
+    X_normalized = Anscombe().transform(X)
     # plotData(X, title="Activity sample before quotient normalisation")
     # plotData(X_normalized, title="Activity sample after quotient normalisation")
 
@@ -142,8 +126,8 @@ if __name__ == "__main__":
     X = createSyntheticActivityData()
     plotData(X, title="Activity sample before quotient normalisation")
 
-    X_normalized = QuotientNormalizer().transform(X)
+    X_normalized = Anscombe().transform(X)
 
     plotData(X_normalized, title="Activity sample after quotient normalisation")
-    print()
+
 

utils/_custom_split.py
@@ -12,6 +12,8 @@ from sys import exit
 from sklearn.model_selection import LeavePOut
 import itertools
 
+from utils._anscombe import Anscombe
+
 np.random.seed(0)
 
 
@@ -89,13 +91,17 @@ class StratifiedLeaveTwoOut:
                     s = np.array(s1[0])
                     if np.unique(s).size != 1:
                         continue
+            if np.unique(y[all_train_idx]).size == 1:
+                warnings.warn("Cannot use fold for training! Only 1 target in FOLD %d" % i)
+                continue
 
             training_idx.append(all_train_idx)
             testing_idx.append(all_test_idx)
             len_check.append(len(test_idx))
             if self.verbose:
                 print("FOLD %d --> \nSAMPLE TRAIN IDX:" % i, np.array(all_train_idx), "\nSAMPLE TEST IDX:", np.array(all_test_idx), "\nTEST TARGET:",
-                      np.unique(y[all_test_idx]), "\nTEST ANIMAL ID:", np.unique(self.animal_ids[all_test_idx]), "\nTRAIN ANIMAL ID:",
+                      np.unique(y[all_test_idx]), "\nTRAIN TARGET:",
+                      np.unique(y[all_train_idx]), "\nTEST ANIMAL ID:", np.unique(self.animal_ids[all_test_idx]), "\nTRAIN ANIMAL ID:",
                       np.unique(self.animal_ids[all_train_idx]))
 
         len_check = np.array(len_check)
@@ -143,6 +149,7 @@ if __name__ == "__main__":
        1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]).reshape(-1, 1)
 
     X = np.array(list(range(y.size))).reshape(-1, 1)
+    X = np.concatenate((X, X), 1)
 
     animal_ids = np.array(['40101310109.0', '40101310109.0', '40101310109.0', '40101310109.0',
        '40101310109.0', '40101310109.0', '40101310109.0', '40101310109.0',
@@ -185,8 +192,8 @@ if __name__ == "__main__":
                   194, 197, 198, 199, 201, 205, 206, 207, 209, 210, 211, 214, 215, 219, 220]
 
     print("DATASET:")
-    dataset = pd.DataFrame(np.hstack((X.reshape(X.size, 1), y.reshape(y.size, 1), animal_ids.reshape(animal_ids.size, 1))))
-    header = ["activity", "target", "animal_id"]
+    dataset = pd.DataFrame(np.hstack((X, y.reshape(y.size, 1), animal_ids.reshape(animal_ids.size, 1))))
+    header = ["a1", "a2", "target", "animal_id"]
     dataset.columns = header
     dataset.to_csv("dummy_dataset_for_cv.csv")
     print(dataset)
@@ -228,6 +235,17 @@ if __name__ == "__main__":
         df_score = pd.DataFrame(scores)
         acc = np.mean(df_score["test_balanced_accuracy_score"].values)
         results.append(acc)
-    print("mean accuracy=", results)
+    print("without anscombe mean accuracy=", results)
+
+    results = []
+    for c in [100]:
+        clf_std_svc = make_pipeline(Anscombe(), SVC(C=c, probability=True, class_weight='balanced'))
+        #cv_std_svc = StratifiedLeaveTwoOut(animal_ids, sample_idx, stratified=F)
+        scores = cross_validate(clf_std_svc, X.copy(), y.copy(), cv=slto, scoring=scoring, n_jobs=-1)
+
+        df_score = pd.DataFrame(scores)
+        acc = np.mean(df_score["test_balanced_accuracy_score"].values)
+        results.append(acc)
+    print("with anscombe mean accuracy=", results)
 
 