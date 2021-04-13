updated pipeline, back transform data to count after imputation, fixed quotient normalisation? , created Anscombe object for make_pipeline.

commands/Samples.py
@@ -301,7 +301,7 @@ class SampleSet:
                         valid_imputed_day += 1
                         cpt = 0
 
-                val = valid_imputed_day > 1
+                val = valid_imputed_day >= 1
 
                 if len(A[np.isnan(A)]) == len(A):
                     val = False

utils/_normalisation.py
@@ -26,7 +26,7 @@ def normalize(X):
     # median values (1 per samples).
     within_median = []
     for msample in X_median:
-        within_median.append(np.median(msample))
+        within_median.append(np.median(msample[msample > 0]))
 
     #step 4 Use the array of medians to scale(multiply) each original sample, which will give all quotient normalized samples.
     qnorm_sample = []