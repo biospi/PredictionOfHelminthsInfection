Quotient normalisation finally correct.

ml_pipeline.py
@@ -1220,9 +1220,8 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
     report_rows_list.append(scores)
     del scores
 
-
-    print('StandardScaler->SVC')
-    clf_svc = make_pipeline(preprocessing.StandardScaler(), SVC(probability=True, class_weight='balanced'))
+    print('Anscombe->Log->SVC')
+    clf_svc = make_pipeline(Anscombe(log=True), SVC(probability=True, class_weight='balanced'))
     scores = cross_validate(clf_svc, X.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
     scores["downsample"] = downsample_false_class
     scores["class0"] = y[y == class_healthy].size
@@ -1238,14 +1237,13 @@ def process_data_frame(stratify, animal_ids, out_dir, data_frame, days, farm_id,
     scores["f1_score0_mean"] = np.mean(scores["test_f1_score0"])
     scores["f1_score1_mean"] = np.mean(scores["test_f1_score1"])
     scores["sampling"] = sampling
-    scores["classifier"] = "StandardScaler->SVC"
+    scores["classifier"] = "Anscombe->Log->SVC"
     scores["classifier_details"] = str(clf_svc).replace('\n', '').replace(" ", '')
-    clf_svc = make_pipeline(preprocessing.StandardScaler(), SVC(probability=True, class_weight='balanced'))
+    clf_svc = make_pipeline(Anscombe(log=True), SVC(probability=True, class_weight='balanced'))
     aucs = make_roc_curve(out_dir, clf_svc, X.copy(), y.copy(), cross_validation_method, param_str)
     scores["roc_auc_score_mean"] = aucs
     report_rows_list.append(scores)
     del scores
-
 
     #####################################################################################################################
 

utils/_anscombe.py
@@ -33,8 +33,9 @@ def anscombe(arr, sigma_sq=0, alpha=1):
 
 
 class Anscombe(TransformerMixin, BaseEstimator):
-    def __init__(self, *, copy=True):
+    def __init__(self, *, log=False, copy=True):
         self.copy = copy
+        self.log = log
 
     def _reset(self):
         """Reset internal data-dependent state of the scaler, if necessary.
@@ -60,7 +61,10 @@ class Anscombe(TransformerMixin, BaseEstimator):
 
     def transform(self, X, copy=None):
         X = check_array(X, accept_sparse='csr')
-        return anscombe(X)
+        if self.log:
+            return np.log(anscombe(X))
+        else:
+            return anscombe(X)
 
     def inverse_transform(self, X):
         return inverse_anscombe(X)

utils/_cwt.py
@@ -84,7 +84,7 @@ def compute_cwt(X, out_dir):
     for activity in tqdm(X):
         y = activity
         w = wavelet.Morlet()
-        coefs, scales, freqs, coi, _, _ = wavelet.cwt(y, 1, wavelet=w, dj=1/24)
+        coefs, scales, freqs, coi, _, _ = wavelet.cwt(y, 1, wavelet=w)
         coefs_cc = np.conj(coefs)
         with np.errstate(divide='ignore'):#ignore numpy divide by zero warning
             #power_cwt = np.log(np.real(np.multiply(coefs, coefs_cc)))
@@ -94,7 +94,7 @@ def compute_cwt(X, out_dir):
         power_masked, coi_line_array = mask_cwt(power_cwt.copy(), coi, scales)
         #power_masked, coi_line_array = power_cwt, []
 
-        plot_cwt_power(out_dir, i, activity, power_masked, coi_line_array, freqs)
+        plot_cwt_power(out_dir, i, activity, power_masked.copy(), coi_line_array, freqs)
         power_flatten_masked = np.array(power_masked.flatten())
         power_flatten_masked = power_flatten_masked[power_flatten_masked != -99]#remove masked values
         cwt.append(power_flatten_masked)
@@ -185,7 +185,7 @@ def get_time_ticks(nticks):
 def plotHeatmap(X, out_dir="", title="Heatmap", filename="heatmap.html", force_xrange=False, head=False):
     # fig = make_subplots(rows=len(transponders), cols=1)
     if head:
-        X = X[:2, :]
+        X = X[:4, :]
     ticks = get_time_ticks(X.shape[1])
     if force_xrange:
         ticks = list(range(X.shape[1]))

utils/_normalisation.py
@@ -9,7 +9,7 @@ from sklearn.utils import check_array
 import numpy as np
 from datetime import datetime, timedelta
 
-from utils.Utils import create_rec_dir
+from utils.Utils import create_rec_dir, anscombe
 
 np.random.seed(0)
 
@@ -18,7 +18,9 @@ def normalize(X, out_dir):
     out_dir_ = out_dir + "_normalisation"
     traces = []
     X = X.astype(np.float)
-    traces.append(plotHeatmap(np.array(X), out_dir_, "STEP 0 | Samples", "0_X_samples.html"))
+    zmin, zmax = np.min(np.log(anscombe(X))), np.max(np.log(anscombe(X)))
+
+    traces.append(plotHeatmap(zmin, zmax, np.array(X).copy(), out_dir_, "STEP 0 | Samples", "0_X_samples.html", y_log=True))
 
     #step 1 find pointwise median sample [median of col1, .... median of col n].
     median_array = np.median(X, axis=0)
@@ -27,46 +29,38 @@ def normalize(X, out_dir):
     #step 2 divide each sample by median array keep div by 0 as NaN!!
     X_median = []
     for x in X:
-        div = np.divide(x, median_array) #return 0 if div by 0!
+        div = np.divide(x, median_array)
         div[div == -np.inf] = np.nan
         div[div == np.inf] = np.nan
+        div[div == 0] = np.nan
         X_median.append(div)
-    traces.append(plotHeatmap(np.array(X_median), out_dir_, "STEP 2 | divide each sample by median array keep div by 0 as NaN", "2_X_median.html", y_log=True))
+    traces.append(plotHeatmap(zmin, zmax, np.array(X_median).copy(), out_dir_, "STEP 2 | divide each sample by median array "
+                                                                        "keep div by 0 as NaN, set 0 to NaN", "2_X_median.html", y_log=True))
 
     #step 3 Within each sample (from iii) store the median value of the sample(excluding 0 value!), which will produce an array of
     # median values (1 per samples).
     within_median = []
     for msample in X_median:
         clean_sample = msample[~np.isnan(msample)]
-        ##clean_sample = clean_sample[clean_sample > 0]
-        clean_sample[clean_sample == 0] = 1
         within_median.append(np.median(clean_sample))
-    traces.append(plotLine([within_median], out_dir_, "STEP 3 | Within each sample (rows from step2) store the median value of the sample(exclude NaN and 0->1), which will produce an array of median values (1 per samples)", "3_within_median.html", x_axis_count=True))
+    traces.append(plotLine([within_median], out_dir_, "STEP 3 | Within each sample (rows from step2) store the median"
+                                                      " value of the sample, which will produce an array of median "
+                                                      "values (1 per samples)", "3_within_median.html", x_axis_count=True, y_log=True))
 
     #step 4 Use the array of medians to scale(divide) each original sample, which will give all quotient normalized samples.
     qnorm_samples = []
     for i, s in enumerate(X):
         qnorm_samples.append(np.divide(s, within_median[i]))
-    traces.append(plotHeatmap(np.array(qnorm_samples), out_dir_, "STEP 4 | Use the array of medians to scale(divide) each original sample, which will give all quotient normalized samples.", "4_qnorm_sample.html"))
+    traces.append(plotHeatmap(zmin, zmax, np.array(qnorm_samples).copy(), out_dir_, "STEP 4 | Use the array of medians"
+                                                                             " to scale(divide) each original sample,"
+                                                                             " which will give all quotient normalized samples.",
+                              "4_qnorm_sample.html", y_log=True))
 
     #step 5 substract step 1 from step 4
     diff = X - np.array(qnorm_samples)
-    traces.append(plotHeatmap(diff, out_dir_, "STEP 5 | Substract step 1 (original samples) from step 4 (quotient normalised samples)", "5_diff.html"))
+    traces.append(plotHeatmap(np.min(diff), np.max(diff), diff, out_dir_, "STEP 5 | Substract step 1 (original samples)"
+                                                                          " from step 4 (quotient normalised samples)", "5_diff.html"))
 
-    # #step 5 Multiply each quotient normalised sample by the total sum off all original samples divided by the sum of
-    # # f all element in the original corresponding sample.
-    # T = np.sum(X.flatten())
-    # t = []
-    # for orig_sample in X:
-    #     t.append(np.sum(orig_sample))
-    #
-    # qnorm_sample_ = []
-    # for i, qqsample in enumerate(qnorm_sample):
-    #     qnorm_sample_.append(qqsample * T/t[i])
-    #
-    # traces.append(plotHeatmap(np.array(qnorm_sample_), out_dir_, "STEP 5 | Multiply each quotient normalised sample by the "
-    #                                                             "total sum off all original samples divided by the sum of "
-    #                                                             "all element in the original corresponding sample.", "5_qnorm_sample_.html"))
     plot_all(traces, out_dir_, title="Quotient Normalisation 5 STEPS")
 
     df_norm = np.array(qnorm_samples)
@@ -169,16 +163,18 @@ def get_time_ticks(nticks):
     return date_list
 
 
-def plotHeatmap(X, out_dir="", title="Heatmap", filename="heatmap.html", y_log=False):
+def plotHeatmap(zmin, zmax, X, out_dir="", title="Heatmap", filename="heatmap.html", y_log=False):
     # fig = make_subplots(rows=len(transponders), cols=1)
     ticks = get_time_ticks(X.shape[1])
     fig = make_subplots(rows=1, cols=1)
     if y_log:
-        X_log = np.log(X)
+        X_log = np.log(anscombe(X))
     trace = go.Heatmap(
             z=X_log if y_log else X,
             x=ticks,
             y=list(range(X.shape[0])),
+            zmin=zmin,
+            zmax=zmax,
             colorscale='Viridis')
     fig.add_trace(trace, row=1, col=1)
     fig.update_layout(title_text=title)

weather_data/src/app.py
@@ -8,6 +8,9 @@ import os
 import pymysql
 import pandas as pd
 from datetime import timedelta
+import numpy as np
+import time
+import datetime
 
 
 def purge_file(filename):
@@ -74,18 +77,47 @@ def get_historical_weather_data(farm_id=None, out_file=None, city=None):
     # days_ = list(set(days))
     # days_ = sorted(days_, key=lambda n: datetime.strptime(n, '%Y-%m-%d'))
     print(len(days_), days_)
+    REQ = 490
+    keys = ["cdb86ad906ff492cbcc111656211304", "4f95d1403a4c4e4485e230252211504", "6b7c8bdf73044f12a82230404211504"]
+    n_d = np.ceil(len(days_)/REQ)
+    print("For 500 req a day you'll need %d days." % n_d)
     URL = "http://api.worldweatheronline.com/premium/v1/past-weather.ashx"
     purge_file(out_file)
     with open(out_file, 'a') as outfile:
-        for i, date in enumerate(days_):
-            PARAMS = {'key': "cdb86ad906ff492cbcc111656211304", 'q': "%s,south+africa" % city,
-                      'date': date, 'tp': 1, 'format': 'json'}
-            r = requests.get(url=URL, params=PARAMS)
-            data = r.json()
-            print(i, len(days_), data)
-            json.dump(data, outfile)
-            outfile.write('\n')
-            sleep(0.5)
+        i = 0
+        while True:
+            if i >= len(days_):
+                break
+
+            for key in keys:
+                cpt = 0
+
+                while True:
+                    if i >= len(days_):
+                        break
+
+                    PARAMS = {'key': key, 'q': "%s,south+africa" % city,
+                              'date': days_[i], 'tp': 1, 'format': 'json'}
+                    r = requests.get(url=URL, params=PARAMS)
+                    data = r.json()
+                    print("progress %d/%d" % (i, len(days_)))
+                    json.dump(data, outfile)
+                    outfile.write('\n')
+                    sleep(0.5)
+                    cpt += 1
+                    i += 1
+                    if cpt >= REQ:
+                        print("use next avail key %s." % key)
+                        break
+
+            print("used all avail keys wait until 2AM...")
+            t = datetime.datetime.today()
+            future = datetime.datetime(t.year, t.month, t.day, 2, 0)
+            if t.hour >= 2:
+                future += datetime.timedelta(days=1)
+            time.sleep((future - t).total_seconds())
+            print("reached max req %d for the day. Wait for next day..." % REQ*len(keys))
+
     print(outfile)
 
 