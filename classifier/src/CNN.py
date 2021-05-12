fixed cwt scales. added pca. used dataset average for centering.

classifier/src/CNN.py
@@ -169,11 +169,7 @@ def load_ucihar_data_(folder):
     return train_signals, train_labels, test_signals, test_labels
 
 
-def load_ucihar_data(folder, X_train, X_test, y_train, y_test ):
-    train_folder = folder + 'train/InertialSignals/'
-    test_folder = folder + 'test/InertialSignals/'
-    labelfile_train = folder + 'train/y_train.txt'
-    labelfile_test = folder + 'test/y_test.txt'
+def load_ucihar_data(X_train, X_test, y_train, y_test ):
     train_signals, test_signals = [], []
     # for input_file in os.listdir(train_folder):
     #     # signal = read_signals_ucihar(train_folder + input_file)
@@ -265,46 +261,8 @@ def load_df_from_datasets(fname, label_col='label'):
     return data_frame_original, data_frame
 
 
-
-if __name__ == "__main__":
-    # dataset = "http://paos.colorado.edu/research/wavelets/wave_idl/sst_nino3.dat"
-    # df_nino = pd.read_table(dataset)
-    # N = df_nino.shape[0]
-    # t0 = 1871
-    # dt = 0.25
-    # time = np.arange(0, N) * dt + t0
-    # signal = df_nino.values.squeeze()
-    #
-    # scales = np.arange(1, 128)
-    # plot_signal_plus_average(time, signal)
-    # plot_fft_plus_power(time, signal)
-    # plot_wavelet(time, signal, scales)
-
-    file = "F:/Data/gen_dataset_debug/delmas_70101200027_1min_famachadays_1_threshold_interpol_30_threshold_zero2nan_480/training_sets/activity_delmas_70101200027_dbft_1_1min_threshi_30_threshz_480.csv"
-    data_frame_original, data_frame = load_df_from_datasets(file)
-    downsample_false_class = True
-    if downsample_false_class:
-        df_true = data_frame[data_frame['label'] == True]
-        df_false = data_frame[data_frame['label'] == False]
-        try:
-            df_false = df_false.sample(df_true.shape[0])
-        except ValueError as e:
-            print(e)
-            sys.exit(-1)
-        data_frame = pd.concat([df_true, df_false], ignore_index=True, sort=False)
-
-    data_frame = get_norm_l2(data_frame)
-
-    y = data_frame['label'].values.flatten()
-    y = y.astype(int)
-    X = data_frame[data_frame.columns[1:data_frame.shape[1] - 1]]
-    test_size = 10
-    X_train_, X_test_, y_train_, y_test_ = train_test_split(X, y, random_state=0, stratify=y, test_size=int(test_size)/100)
-
-
-    folder_ucihar = 'F:/Data/CNN/UCI HAR Dataset/'
-    # uci_har_signals_train, uci_har_labels_train, uci_har_signals_test, uci_har_labels_test = load_ucihar_data_(folder_ucihar)
-    uci_har_signals_train, uci_har_labels_train, uci_har_signals_test, uci_har_labels_test = load_ucihar_data(folder_ucihar, X_train_, X_test_, y_train_, y_test_ )
+def cnn2d(X_train_, X_test_, y_train_, y_test_ ):
+    uci_har_signals_train, uci_har_labels_train, uci_har_signals_test, uci_har_labels_test = load_ucihar_data(X_train_, X_test_, y_train_, y_test_ )
     # print(uci_har_labels_test)
 
     scales = range(1, X_train_.shape[1])
@@ -423,6 +381,48 @@ if __name__ == "__main__":
     # print("CNN                                     ")
     # y_test = np.argmax(y_test, axis=1)
     # print(classification_report(y_test, y_pred_bool))
+
+
+if __name__ == "__main__":
+    # dataset = "http://paos.colorado.edu/research/wavelets/wave_idl/sst_nino3.dat"
+    # df_nino = pd.read_table(dataset)
+    # N = df_nino.shape[0]
+    # t0 = 1871
+    # dt = 0.25
+    # time = np.arange(0, N) * dt + t0
+    # signal = df_nino.values.squeeze()
+    #
+    # scales = np.arange(1, 128)
+    # plot_signal_plus_average(time, signal)
+    # plot_fft_plus_power(time, signal)
+    # plot_wavelet(time, signal, scales)
+
+    file = "F:/Data/gen_dataset_debug/delmas_70101200027_1min_famachadays_1_threshold_interpol_30_threshold_zero2nan_480/training_sets/activity_delmas_70101200027_dbft_1_1min_threshi_30_threshz_480.csv"
+    data_frame_original, data_frame = load_df_from_datasets(file)
+    downsample_false_class = True
+    if downsample_false_class:
+        df_true = data_frame[data_frame['label'] == True]
+        df_false = data_frame[data_frame['label'] == False]
+        try:
+            df_false = df_false.sample(df_true.shape[0])
+        except ValueError as e:
+            print(e)
+            sys.exit(-1)
+        data_frame = pd.concat([df_true, df_false], ignore_index=True, sort=False)
+
+    data_frame = get_norm_l2(data_frame)
+
+    y = data_frame['label'].values.flatten()
+    y = y.astype(int)
+    X = data_frame[data_frame.columns[1:data_frame.shape[1] - 1]]
+    test_size = 10
+    X_train_, X_test_, y_train_, y_test_ = train_test_split(X, y, random_state=0, stratify=y, test_size=int(test_size)/100)
+    cnn2d(X_train_, X_test_, y_train_, y_test_)
+
+
+    folder_ucihar = 'F:/Data/CNN/UCI HAR Dataset/'
+    # uci_har_signals_train, uci_har_labels_train, uci_har_signals_test, uci_har_labels_test = load_ucihar_data_(folder_ucihar)
+
 
 
 

cnn/cnn.py
@@ -1,4 +1,10 @@
 import warnings
+import keras
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.callbacks import History
+import pywt
 from numpy import mean
 from numpy import std
 from numpy import dstack
@@ -16,6 +22,7 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import make_blobs
 
+from utils.Utils import create_rec_dir
 from utils._custom_split import StratifiedLeaveTwoOut
 import sklearn
 import matplotlib.pyplot as plt
@@ -26,7 +33,7 @@ from utils.visualisation import mean_confidence_interval, plot_roc_range
 from keras.utils.vis_utils import plot_model
 import time
 import os
-# os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz/bin/'
 
 
 class Score:
@@ -121,7 +128,7 @@ def summarize_results(scores):
     print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
 
 
-def evaluate_model(ax, trainX, trainy, testX, testy, verbose=0, epochs=1000, batch_size=8):
+def evaluate_model(i, out_dir, ax, trainX, trainy, testX, testy, verbose=0, epochs=1000, batch_size=8):
     X_train, X_test, y_train, y_test = format(trainX, trainy, testX, testy)
     n_timesteps, n_features, n_outputs = X_train.shape[1], X_train.shape[2], y_train.shape[1]
     model = Sequential()
@@ -142,10 +149,39 @@ def evaluate_model(ax, trainX, trainy, testX, testy, verbose=0, epochs=1000, bat
         metrics.Recall(name='recall'),
         metrics.AUC(name='auc'),
     ]
-    # plot_model(model, show_shapes=True, to_file='multichannel.png')
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=METRICS)
+    plot_model(model, show_shapes=True, to_file='multichannel.png')
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=METRICS)
     # fit network
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    # list all data in history
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['auc'])
+    ##plt.plot(history.history['val_auc'])
+    plt.title('model auc for fold %d' % i)
+    plt.ylabel('auc')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    path = "%s/model/" % out_dir
+    create_rec_dir(path)
+    final_path = '%s/%s' % (path, 'fold%d_modelauc.png' % (i))
+    print(final_path)
+    plt.savefig(final_path)
+    plt.clf()
+
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    ##plt.plot(history.history['val_loss'])
+    plt.title('model loss for fold %d' % i)
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    path = "%s/model/" % out_dir
+    create_rec_dir(path)
+    final_path = '%s/%s' % (path, 'fold%d_modelloss.png' % (i))
+    print(final_path)
+    plt.savefig(final_path)
+
     # evaluate model
     baseline_results = model.evaluate(X_test, y_test, batch_size=batch_size, verbose=0)
     # for name, value in zip(model.metrics_names, baseline_results):
@@ -178,6 +214,183 @@ def format(trainX, trainy, testX, testy):
     return X_train, X_test, y_train, y_test
 
 
+def formatDataFor2DCnn(X_train, X_test, y_train, y_test):
+    train_signals, test_signals = [], []
+    # for input_file in os.listdir(train_folder):
+    #     # signal = read_signals_ucihar(train_folder + input_file)
+
+    train_signals.append(X_train)
+    train_signals = np.transpose(np.array(train_signals), (1, 2, 0))
+    # for input_file in os.listdir(test_folder):
+    #     signal = read_signals_ucihar(test_folder + input_file)
+    #     test_signals.append(signal)
+    test_signals.append(X_test)
+    test_signals = np.transpose(np.array(test_signals), (1, 2, 0))
+
+    train_labels = y_train.tolist()
+    test_labels = y_test.tolist()
+    return train_signals, train_labels, test_signals, test_labels
+
+
+def run2DCnn(epochs, cross_validation_method, X, y, class_healthy, class_unhealthy, steps, days, farm_id, sampling, label_series, downsample_false_class, output_dir):
+    start_time = time.time()
+    fig, ax = plt.subplots()
+    mean_fpr = np.linspace(0, 1, 100)
+    tprs = []
+    aucs = []
+    test_balanced_accuracy_score = []
+    test_precision_score0 = []
+    test_precision_score1 = []
+    test_recall_score0 = []
+    test_recall_score1 = []
+    test_f1_score0 = []
+    test_f1_score1 = []
+    i = 0
+    for train_index, test_index in cross_validation_method.split(X, y):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        roc_auc, fpr, tpr, acc, p0, p1, r0, r1, f0, f1 = evaluate2DCnn(i, output_dir, ax, X_train, y_train, X_test, y_test, epochs=epochs)
+        i += 1
+        if np.isnan(roc_auc):
+            warnings.warn("classifier returned the same target for all testing samples.")
+            continue
+        interp_tpr = np.interp(mean_fpr, fpr, tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(roc_auc)
+        test_balanced_accuracy_score.append(acc)
+        test_precision_score0.append(p0)
+        test_precision_score1.append(p1)
+        test_recall_score0.append(r0)
+        test_recall_score1.append(r1)
+        test_f1_score0.append(f0)
+        test_f1_score1.append(f1)
+
+    mean_auc = plot_roc_range(ax, tprs, mean_fpr, aucs, output_dir, steps+"_CNN", fig)
+    # fig.show()
+    plt.close(fig)
+    plt.clf()
+
+    Score(y, class_healthy, class_unhealthy, steps, days, farm_id, test_balanced_accuracy_score,
+                 test_precision_score0, test_precision_score1, test_recall_score0, test_recall_score1,
+                 test_f1_score0, test_f1_score1, sampling, mean_auc, label_series,
+                 cross_validation_method, start_time, output_dir, downsample_false_class)
+
+
+def evaluate2DCnn(i, out_dir, ax, X_train_, y_train_, X_test_, y_test_, verbose=0, epochs=1000, batch_size=8):
+    Xtrain, ytrain, Xtest, ytest = formatDataFor2DCnn(X_train_, X_test_, y_train_, y_test_)
+
+    scales = range(1, X_train_.shape[1])
+    waveletname = 'morl'
+    train_size = Xtrain.shape[0]
+    test_size = Xtest.shape[0]
+    train_data_cwt = np.ndarray(shape=(train_size, Xtrain.shape[1]-1, Xtrain.shape[1], 1))
+
+    for ii in range(0, train_size):
+        print(ii, train_size)
+        jj = 0
+        signal = Xtrain[ii, :, jj]
+        coeff, freq = pywt.cwt(signal, scales, waveletname, 1)
+        coeff_ = coeff[:, :X_train_.shape[1]]
+        train_data_cwt[ii, :, :, jj] = coeff_
+
+    test_data_cwt = np.ndarray(shape=(test_size, X_test_.shape[1]-1, X_test_.shape[1], 1))
+    for ii in range(0, test_size):
+        print(ii, test_size)
+        jj = 0
+        signal = Xtest[ii, :, jj]
+        coeff, freq = pywt.cwt(signal, scales, waveletname, 1)
+        coeff_ = coeff[:, :X_train_.shape[1]]
+        test_data_cwt[ii, :, :, jj] = coeff_
+
+
+    ytrain = list(map(lambda x: int(x) - 1, ytrain))
+    ytest = list(map(lambda x: int(x) - 1, ytest))
+
+    x_train = train_data_cwt
+    y_train = list(ytrain[:train_size])
+    x_test = test_data_cwt
+    y_test = list(ytest[:test_size])
+
+    img_x = coeff_.shape[0]
+    img_y = coeff_.shape[1]
+    img_z = 1
+    input_shape = (img_x, img_y, img_z)
+
+    batch_size = 5
+    num_classes = 2
+    epochs = 10
+
+    x_train = x_train.astype('float16')
+    x_test = x_test.astype('float16')
+
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(5, 5), strides=(5, 5),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(10, 10)))
+    model.add(Conv2D(64, (5, 5), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1000, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+
+    METRICS = [
+        keras.metrics.TruePositives(name='tp'),
+        keras.metrics.FalsePositives(name='fp'),
+        keras.metrics.TrueNegatives(name='tn'),
+        keras.metrics.FalseNegatives(name='fn'),
+        keras.metrics.BinaryAccuracy(name='accuracy'),
+        keras.metrics.Precision(name='precision'),
+        keras.metrics.Recall(name='recall'),
+        keras.metrics.AUC(name='auc'),
+    ]
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adam(),
+                  metrics=METRICS)
+
+    history = model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1)
+    # list all data in history
+    print(history.history.keys())
+    # summarize history for accuracy
+    plt.plot(history.history['auc'])
+    ##plt.plot(history.history['val_auc'])
+    plt.title('model auc for fold %d' % i)
+    plt.ylabel('auc')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    path = "%s/model/" % out_dir
+    create_rec_dir(path)
+    final_path = '%s/%s' % (path, 'fold%d_modelauc.png' % (i))
+    print(final_path)
+    plt.savefig(final_path)
+    plt.clf()
+
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    ##plt.plot(history.history['val_loss'])
+    plt.title('model loss for fold %d' % i)
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    path = "%s/model/" % out_dir
+    create_rec_dir(path)
+    final_path = '%s/%s' % (path, 'fold%d_modelloss.png' % (i))
+    print(final_path)
+    plt.savefig(final_path)
+
+    # evaluate model
+    baseline_results = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=0)
+    roc_auc, fpr, tpr, report, acc, p0, p1, r0, r1, f0, f1 = plot_roc(ax, model, x_test, y_test)
+    return roc_auc, fpr, tpr, acc, p0, p1, r0, r1, f0, f1
+
+
 def run1DCnn(epochs, cross_validation_method, X, y, class_healthy, class_unhealthy, steps, days, farm_id, sampling, label_series, downsample_false_class, output_dir):
     start_time = time.time()
     fig, ax = plt.subplots()
@@ -191,10 +404,12 @@ def run1DCnn(epochs, cross_validation_method, X, y, class_healthy, class_unhealt
     test_recall_score1 = []
     test_f1_score0 = []
     test_f1_score1 = []
+    i = 0
     for train_index, test_index in cross_validation_method.split(X, y):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        roc_auc, fpr, tpr, acc, p0, p1, r0, r1, f0, f1 = evaluate_model(ax, X_train, y_train, X_test, y_test, epochs=epochs)
+        roc_auc, fpr, tpr, acc, p0, p1, r0, r1, f0, f1 = evaluate_model(i, output_dir, ax, X_train, y_train, X_test, y_test, epochs=epochs)
+        i += 1
         if np.isnan(roc_auc):
             warnings.warn("classifier returned the same target for all testing samples.")
             continue

utils/_cwt.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib
 from matplotlib.ticker import ScalarFormatter
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import check_array
 from sklearn.base import TransformerMixin, BaseEstimator
 import pycwt as wavelet
@@ -111,16 +112,18 @@ def make_roc_curve(out_dir, classifier, X, y, cv, param_str):
     return mean_auc
 
 
-def plot_cwt_power(step_slug, out_dir, i, activity, activity_centered, power_masked, coi_line_array, freqs, format_xaxis=True):
-    wavelength = 1/freqs
+def plot_cwt_power(step_slug, out_dir, i, activity, activity_centered, power_masked, coi_line_array, freqs, scales,
+                   format_xaxis=True):
+    wavelength = 1 / freqs
     plt.clf()
     fig, axs = plt.subplots(1, 2, figsize=(19.20, 7.20))
-    #fig.suptitle("Signal , CWT", fontsize=18)
+    # fig.suptitle("Signal , CWT", fontsize=18)
     ticks = list(range(len(activity)))
     if format_xaxis:
         ticks = get_time_ticks(len(activity))
     axs[0].plot(ticks, activity, label='activity')
-    axs[0].plot(ticks, activity_centered, label='activity centered (signal - average of signal (=%.2f))' % np.average(activity))
+    axs[0].plot(ticks, activity_centered,
+                label='activity centered (signal - average of signal (=%.2f))' % np.average(activity))
     axs[0].legend(loc="upper right")
     axs[0].set_title("Time domain signal")
 
@@ -135,17 +138,17 @@ def plot_cwt_power(step_slug, out_dir, i, activity, activity_centered, power_mas
     with np.errstate(invalid='ignore'):  # ignore numpy divide by zero warning
         if step_slug == "QN_CWT_ANSCOMBE_LOG":
             power_masked = np.log(anscombe(power_masked))
-        pos = axs[1].imshow(power_masked)
+        pos = axs[1].imshow(power_masked, extent=[0, len(activity), len(scales), 1])
         fig.colorbar(pos, ax=axs[1])
-    if(len(coi_line_array) > 0):
-        axs[1].plot(coi_line_array, linestyle="--", linewidth=0, c="white")#todo fix xratio
+
+    #axs[1].plot(coi_line_array, linestyle="--", linewidth=1, c="red")  # todo fix xratio
     axs[1].set_aspect('auto')
     axs[1].set_title("CWT")
     axs[1].set_xlabel("Time in minute")
     if format_xaxis:
         axs[1].set_xlabel("Time")
     axs[1].set_ylabel("Wave length of wavelet (in minute)")
-    #axs[1].set_yscale('log')
+    axs[1].set_yscale('log')
 
     if format_xaxis:
         n_x_ticks = axs[1].get_xticks().shape[0]
@@ -154,21 +157,22 @@ def plot_cwt_power(step_slug, out_dir, i, activity, activity_centered, power_mas
         labels_[:] = labels_[0]
         axs[1].set_xticklabels(labels_)
 
-    # n_y_ticks = axs[1].get_yticks().shape[0]-2
-    cwty = axs[1].get_yticks()
-    n_y_ticks = cwty.shape[0] - len([x for x in cwty if x < 0])
-
-    labels_wl = ["%.2f" % item for item in wavelength]
-    # print(labels)
-    labels_wt = np.array(labels_wl)[list(range(1, len(labels_wl), int(len(labels_wl) / n_y_ticks)))][1:]
-    #new_lab = [matplotlib.text.Text(0, float(labels_wt[0]), labels_wt[0])]
-    new_lab = []
-    for ii, l in enumerate(labels_wt):
-        new_lab.append(matplotlib.text.Text(cwty[ii], float(l), l))
-    #new_lab[-1] = matplotlib.text.Text(8, float(l), l)
-    axs[1].set_yticklabels(new_lab)
-
-    axs[1].tick_params(axis='y', which='both', colors='black')
+    # # n_y_ticks = axs[1].get_yticks().shape[0]-2
+    # cwty = axs[1].get_yticks()
+    # n_y_ticks = cwty.shape[0] - len([x for x in cwty if x < 0])
+    #
+    # labels_wl = ["%.2f" % item for item in wavelength]
+    # # print(labels)
+    # labels_wt = np.array(labels_wl)[list(range(1, len(labels_wl), int(len(labels_wl) / n_y_ticks)))][1:]
+    # # new_lab = [matplotlib.text.Text(0, float(labels_wt[0]), labels_wt[0])]
+    # new_lab = []
+    # for ii, l in enumerate(labels_wt):
+    #     new_lab.append(matplotlib.text.Text(cwty[ii], float(l), l))
+    # # new_lab[-1] = matplotlib.text.Text(8, float(l), l)
+    # axs[1].set_yticklabels(new_lab)
+    #
+    # axs[1].tick_params(axis='y', which='both', colors='black')
+    # axs[1].set_yticks(wavelength)
 
     filename = "%d_%s_cwt.png" % (i, step_slug)
     filepath = "%s/%s" % (out_dir, filename)
@@ -180,34 +184,25 @@ def plot_cwt_power(step_slug, out_dir, i, activity, activity_centered, power_mas
     plt.close(fig)
 
 
-def mask_cwt(cwt, coi, scales, turn_off=False):
-    if turn_off:
-        return cwt
-    # print("masking cwt...")
-
-    coi_line = []
-    for j in range(cwt.shape[1]):
-        for i, s in enumerate(scales):
-            c = coi[j]
-            if s > c:
-                cwt[i:, j] = np.nan
-                coi_line.append(i)
-                break
-
-    return cwt, coi_line
+def mask_cwt(cwt, coi):
+    mask = np.ones(cwt.shape)
+    for i in range(cwt.shape[1]):
+        mask[int(coi[i]):, i] = np.nan
+    cwt = mask * cwt
+    return cwt
 
 
 def CWTVisualisation(step_slug, graph_outputdir, shape, freqs, coi_line_array,
-                  df_timedomain, df_cwt,
-                  class_healthy_label, class_unhealthy_label,
-                  class_healthy, class_unhealthy):
+                     df_timedomain, df_cwt,
+                     class_healthy_label, class_unhealthy_label,
+                     class_healthy, class_unhealthy):
     idx_healthy = df_timedomain[df_timedomain["target"] == class_healthy].index.tolist()
     idx_unhealthy = df_timedomain[df_timedomain["target"] == class_unhealthy].index.tolist()
     h_m = np.mean(df_cwt.loc[idx_healthy].values, axis=0).reshape(shape)
     uh_m = np.mean(df_cwt.loc[idx_unhealthy].values, axis=0).reshape(shape)
     plot_cwt_power_sidebyside(step_slug, True, class_healthy_label, class_unhealthy_label, class_healthy,
-                                  class_unhealthy, idx_healthy, idx_unhealthy, coi_line_array, df_timedomain,
-                                  graph_outputdir, h_m, uh_m, freqs, ntraces=2)
+                              class_unhealthy, idx_healthy, idx_unhealthy, coi_line_array, df_timedomain,
+                              graph_outputdir, h_m, uh_m, freqs, ntraces=2)
 
 
 def check_scale_spacing(scales):
@@ -220,28 +215,61 @@ def check_scale_spacing(scales):
     return np.mean(spaces)
 
 
-def center_signal(y):
-    y_centered = y - np.average(y)
+def center_signal(y, avg):
+    y_centered = y - avg
     return y_centered
 
 
-def cwt_power(activity, out_dir, i=0, step_slug="CWT_POWER", format_xaxis=None):
-    y = center_signal(activity)
-    wavelenght = len(activity) #nday wavelenght in minutes
-    f0 = 1 / wavelenght
+def simple_example():
+    num_steps = 512
+    x = np.arange(num_steps)
+    y = np.sin(2 * np.pi * x / 32)
+    delta_t = 1
+    scales = np.arange(1, num_steps + 1)
+    # pycwt
+    w = wavelet.Morlet(0.1)
+    freqs = 1 / (w.flambda() * scales)
+    test = 1 / freqs
+
+    coefs, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(y, delta_t, wavelet=w, freqs=freqs)
+    coefs_cc = np.conj(coefs)
+    power_cwt = np.real(np.multiply(coefs, coefs_cc))
+
+    fig, axs = plt.subplots(1, 2, figsize=(19.20, 7.20))
+    axs[0].plot(y, label='signal')
+    axs[0].set(xlabel="Time", ylabel="amplitude")
+
+    pos = axs[1].imshow(power_cwt)
+    fig.colorbar(pos, ax=axs[1])
+    axs[1].set_aspect('auto')
+    axs[1].set_title("CWT")
+    axs[1].set_xlabel("Time")
+    #axs[1].set_yscale('log')
+    fig.show()
+    fig.clear()
+    plt.close(fig)
+
+
+def cwt_power(activity, out_dir, i=0, step_slug="CWT_POWER", format_xaxis=None, avg=0):
+    y = center_signal(activity, avg)
+    delta_t = 1
+    scales = np.arange(1, len(y) + 1)
+    freqs = 1 / (scales)
+    f0 = 2*np.pi*freqs[-1]
     w = wavelet.Morlet(f0)
-    #w = wavelet.MexicanHat()
-    coefs, scales, freqs, coi, _, _ = wavelet.cwt(y, 0.1, wavelet=w, dj=1./30.)
-    #mean_scale_space = check_scale_spacing(scales)
+    coefs, _, _, coi, fft, fftfreqs = wavelet.cwt(y, delta_t, wavelet=w, freqs=freqs)
+    coi = np.interp(coi, (coi.min(), coi.max()), (0, len(scales))) #todo fix weird hack
+
     coefs_cc = np.conj(coefs)
     with np.errstate(divide='ignore'):  # ignore numpy divide by zero warning
         # power_cwt = np.log(np.real(np.multiply(coefs, coefs_cc)))
         power_cwt = np.real(np.multiply(coefs, coefs_cc))
     # power_cwt[power_cwt == -np.inf] = 0  # todo check why inf output
-    power_masked, coi_line_array = mask_cwt(power_cwt.copy(), coi, scales)
+    power_masked = mask_cwt(power_cwt.copy(), coi)
     shape = power_cwt.shape
     # power_masked, coi_line_array = power_cwt, []
-    plot_cwt_power(step_slug, out_dir, i, activity, y, power_masked.copy(), coi_line_array, freqs, format_xaxis=format_xaxis)
+    plot_cwt_power(step_slug, out_dir, i, activity, y, power_masked.copy(), coi, freqs, scales,
+                   format_xaxis=format_xaxis)
     return power_masked, freqs, coi, shape
 
 
@@ -253,10 +281,10 @@ def compute_cwt(X, out_dir, step_slug, format_xaxis=None):
     cwt_full = []
     i = 0
     for activity in tqdm(X):
-        power_masked, freqs, coi, shape = cwt_power(activity, out_dir, i, step_slug, format_xaxis)
+        power_masked, freqs, coi, shape = cwt_power(activity, out_dir, i, step_slug, format_xaxis, avg=np.average(X))
         power_flatten_masked = np.array(power_masked.flatten())
         cwt_full.append(power_flatten_masked)
-        power_flatten_masked = power_flatten_masked[~np.isnan(power_flatten_masked)]#remove masked values
+        power_flatten_masked = power_flatten_masked[~np.isnan(power_flatten_masked)]  # remove masked values
         cwt.append(power_flatten_masked)
         i += 1
     cwt = np.array(cwt)
@@ -291,7 +319,7 @@ class CWT(TransformerMixin, BaseEstimator):
         return self
 
     def transform(self, X, copy=None):
-        #copy = copy if copy is not None else self.copy
+        # copy = copy if copy is not None else self.copy
         X = check_array(X, accept_sparse='csr')
         cwt, cwt_full, freqs, coi, shape = compute_cwt(X, self.out_dir, self.step_slug, self.format_xaxis)
         self.freqs = freqs
@@ -315,8 +343,8 @@ def createSyntheticActivityData(n_samples=4):
     samples_path = "F:/Data2/dataset_gain_7day/activity_delmas_70101200027_dbft_7_1min.csv"
     df = pd.read_csv(samples_path, header=None)
     df = df.fillna(0)
-    crop = -4 - int(df.shape[1]/1.1)
-    activity = df.iloc[259, 9353: 9353+60*6].values
+    crop = -4 - int(df.shape[1] / 1.1)
+    activity = df.iloc[259, :-4].values.astype(int)
 
     dataset = []
     for j in range(n_samples):
@@ -364,10 +392,10 @@ def plotHeatmap(X, out_dir="", title="Heatmap", filename="heatmap.html", force_x
 
     fig = make_subplots(rows=1, cols=1)
     trace = go.Heatmap(
-            z=X,
-            x=ticks,
-            y=list(range(X.shape[0])),
-            colorscale='Viridis')
+        z=X,
+        x=ticks,
+        y=list(range(X.shape[0])),
+        colorscale='Viridis')
     fig.add_trace(trace, row=1, col=1)
     fig.update_layout(title_text=title)
     # fig.show()
@@ -380,21 +408,21 @@ def plotHeatmap(X, out_dir="", title="Heatmap", filename="heatmap.html", force_x
 
 def createSinWave(f, time):
     t = np.linspace(0, time, int(time))
-    y = np.sin(2. * np.pi * t * f)*100
+    y = np.sin(2. * np.pi * t * f) * 100
     return y.astype(float)
 
 
 def createPoisson(time, s):
-    seed = np.ceil(random.random()*2)+1
+    seed = np.ceil(random.random() * 2) + 1
     noise = np.random.poisson(int(seed), size=int(time))
-    y = noise*s
+    y = noise * s
     return y
 
 
 def createNormal(f, time, s):
     seed = np.ceil(f * 2) + 1
     noise = np.random.normal(seed, size=int(time))
-    y = noise*s
+    y = noise * s
     return y
 
 
@@ -435,14 +463,18 @@ def creatSin(freq, time):
 
 if __name__ == "__main__":
     print("********CWT*********")
+    # simple_example()
+
 
     X = []
     for i in np.arange(5, 100, 5):
         X.append(creatSin(i, 1440))
     X = np.array(X)
+    X = np.array(createSyntheticActivityData())
     X_CWT = CWT(out_dir="F:/Data2/_cwt_unit_before", format_xaxis=False).transform(X)
+    exit()
 
-    for d in [(60*60*24*1)/60, (60*60*24*7)/60]:
+    for d in [(60 * 60 * 24 * 1) / 60, (60 * 60 * 24 * 7) / 60]:
 
         signal10 = []
         for _ in range(60):
@@ -452,22 +484,22 @@ if __name__ == "__main__":
             signal2.append(creatSin(2 + np.random.random() / 100, d))
 
         for out_dir in ["F:/Data2/_cwt_debug_poisson_%d/" % d, "F:/Data2/_cwt_debug_normal_%d/" % d]:
-            #X = np.array(createSyntheticActivityData()
-            #X, targets = createSinWaves(d)
-            #X, targets = createPoisonWaves(d)
+            # X = np.array(createSyntheticActivityData()
+            # X, targets = createSinWaves(d)
+            # X, targets = createPoisonWaves(d)
             if "poisson" in out_dir:
                 X, targets = createPoisonWaves(d, signal10, signal2)
 
             if "normal" in out_dir:
                 X, targets = createNormalWaves(d, signal10, signal2)
 
-            #X = pd.concat([pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X)], axis=1)
-            #X.columns = list(range(X.shape[1]))
-            #plotLine(X, out_dir=out_dir, title="Activity samples", filename="X.html")
+            # X = pd.concat([pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X), pd.DataFrame(X)], axis=1)
+            # X.columns = list(range(X.shape[1]))
+            # plotLine(X, out_dir=out_dir, title="Activity samples", filename="X.html")
 
             X_CWT = CWT(out_dir=out_dir, format_xaxis=False).transform(X)
 
-            #plotLine(X_CWT, out_dir=out_dir, title="CWT samples", filename="CWT.html")
+            # plotLine(X_CWT, out_dir=out_dir, title="CWT samples", filename="CWT.html")
             print("********************")
             report_rows_list = []
             y = np.array(targets)
@@ -513,7 +545,8 @@ if __name__ == "__main__":
 
             print('CWT->SVC')
             clf_svc = make_pipeline(SVC(probability=True, class_weight='balanced'))
-            scores = cross_validate(clf_svc, X_CWT.copy(), y.copy(), cv=cross_validation_method, scoring=scoring, n_jobs=-1)
+            scores = cross_validate(clf_svc, X_CWT.copy(), y.copy(), cv=cross_validation_method, scoring=scoring,
+                                    n_jobs=-1)
             scores["class0"] = y[y == class_healthy].size
             scores["class1"] = y[y == class_unhealthy].size
             scores["option"] = "CWT"
@@ -554,20 +587,20 @@ if __name__ == "__main__":
             print(df)
 
             t4 = "AUC performance of different inputs<br>Days=%d class0=%d %s class1=%d %s" % (
-            df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
-            df["class_1_label"].values[0])
+                df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
+                df["class_1_label"].values[0])
 
             t3 = "Accuracy performance of different inputs<br>Days=%d class0=%d %s class1=%d %s" % (
-            df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
-            df["class_1_label"].values[0])
+                df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
+                df["class_1_label"].values[0])
 
             t1 = "Precision class0 performance of different inputs<br>Days=%d class0=%d %s class1=%d %s" % (
-            df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
-            df["class_1_label"].values[0])
+                df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
+                df["class_1_label"].values[0])
 
             t2 = "Precision class1 performance of different inputs<br>Days=%d class0=%d %s class1=%d %s" % (
-            df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
-            df["class_1_label"].values[0])
+                df["days"].values[0], df["class0"].values[0], df["class_0_label"].values[0], df["class1"].values[0],
+                df["class_1_label"].values[0])
 
             fig = make_subplots(rows=4, cols=1, subplot_titles=(t1, t2, t3, t4))
 
@@ -581,7 +614,8 @@ if __name__ == "__main__":
             fig.update_yaxes(range=[0, 1], row=3, col=1)
             fig.update_yaxes(range=[0, 1], row=4, col=1)
 
-            fig.add_shape(type="line", x0=-0.0, y0=0.920, x1=1.0, y1=0.920, line=dict(color="LightSeaGreen", width=4, dash="dot",))
+            fig.add_shape(type="line", x0=-0.0, y0=0.920, x1=1.0, y1=0.920,
+                          line=dict(color="LightSeaGreen", width=4, dash="dot", ))
 
             fig.add_shape(type="line", x0=-0.0, y0=0.640, x1=1.0, y1=0.640,
                           line=dict(color="LightSeaGreen", width=4, dash="dot", ))
@@ -610,4 +644,3 @@ if __name__ == "__main__":
             print(filename)
             fig.write_html(filename)
             # fig.show()
-

utils/_normalisation.py
@@ -24,7 +24,7 @@ def normalize(X, out_dir):
     #step 1 find pointwise median sample [median of col1, .... median of col n].
     median_array = np.median(X, axis=0)
     traces.append(plotLine([median_array], out_dir_, "STEP 1 | find pointwise median sample [median of col1, .... median of col n]", "1_median_array.html"))
-    cwt_power(median_array, out_dir, step_slug="HERD")
+    cwt_power(median_array, out_dir, step_slug="HERD", avg=np.average(median_array))
 
     #step 2 divide each sample by median array keep div by 0 as NaN!!
     X_median = []

utils/visualisation.py
@@ -250,7 +250,7 @@ def plot_cwt_power_sidebyside(step_slug, output_samples, class_healthy_label, cl
     plt.clf()
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(19.20, 7.20))
     # fig.suptitle(title, fontsize=18)
-    wavelength = 1 / freqs
+    #wavelength = 1 / freqs
 
     df_healthy = df_timedomain[df_timedomain["target"] == class_healthy].iloc[:, :-meta_size].values
     df_unhealthy = df_timedomain[df_timedomain["target"] == class_unhealthy].iloc[:, :-meta_size].values
@@ -296,15 +296,15 @@ def plot_cwt_power_sidebyside(step_slug, output_samples, class_healthy_label, cl
     # labels = ["%.f" % item for item in wavelength]
     # labels_ = np.array(labels)[list(range(1, len(labels), int(len(labels) / n_y_ticks)))]
     # ax3.set_yticklabels(labels_)
-    cwty = ax3.get_yticks()
-    n_y_ticks = cwty.shape[0] - len([x for x in cwty if x < 0])
-    labels = ["%.2f" % item for item in wavelength]
+    #cwty = ax3.get_yticks()
+    #n_y_ticks = cwty.shape[0] - len([x for x in cwty if x < 0])
+    #labels = ["%.2f" % item for item in wavelength]
     # print(labels)
-    labels_ = np.array(labels)[list(range(1, len(labels), int(len(labels) / n_y_ticks)))][1:]
-    new_lab = []
-    for ii, l in enumerate(labels_):
-        new_lab.append(matplotlib.text.Text(cwty[ii], float(l), l))
-    ax3.set_yticklabels(new_lab)
+    #labels_ = np.array(labels)[list(range(1, len(labels), int(len(labels) / n_y_ticks)))][1:]
+    #new_lab = []
+    #for ii, l in enumerate(labels_):
+    #    new_lab.append(matplotlib.text.Text(cwty[ii], float(l), l))
+    #ax3.set_yticklabels(new_lab)
 
     n_x_ticks = ax3.get_xticks().shape[0]
     labels_ = [item.strftime("%H:00") for item in ticks]
@@ -330,8 +330,6 @@ def plot_cwt_power_sidebyside(step_slug, output_samples, class_healthy_label, cl
     pos1 = ax4.imshow(p1, vmin=vmin, vmax=vmax)
     fig.colorbar(pos1, ax=ax4)
 
-
-
     # ax3.set_ylim([min(imshow_y_axis), max(imshow_y_axis)])
     # ax4.set_ylim([min(imshow_y_axis), max(imshow_y_axis)])
 
@@ -347,13 +345,13 @@ def plot_cwt_power_sidebyside(step_slug, output_samples, class_healthy_label, cl
     #ax4.set_yscale('log')
 
     #n_y_ticks = ax4.get_yticks().shape[0] - 1
-    labels = ["%.2f" % item for item in wavelength]
+    #labels = ["%.2f" % item for item in wavelength]
     # print(labels)
-    labels_ = np.array(labels)[list(range(1, len(labels), int(len(labels) / n_y_ticks)))][1:]
-    new_lab = []
-    for ii, l in enumerate(labels_):
-        new_lab.append(matplotlib.text.Text(cwty[ii], float(l), l))
-    ax4.set_yticklabels(new_lab)
+    #labels_ = np.array(labels)[list(range(1, len(labels), int(len(labels) / n_y_ticks)))][1:]
+    #new_lab = []
+    #for ii, l in enumerate(labels_):
+    #    new_lab.append(matplotlib.text.Text(cwty[ii], float(l), l))
+    #ax4.set_yticklabels(new_lab)
 
     n_x_ticks = ax4.get_xticks().shape[0]
     labels_ = [item.strftime("%H:00") for item in ticks]